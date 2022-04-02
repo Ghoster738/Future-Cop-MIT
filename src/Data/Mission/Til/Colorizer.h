f72a1aba6e03e33d661e686ae0fe8f5d57182090
GhostAgent738 <Ghoster738@users.noreply.github.com>
2022-04-02 13:05:57 -0700

    The tile_shade.json is removed.
    tile_shade.json is useless, because it does not represent how the
    tile shading works.
    
    My new assumption is that the shaded tile system is more complex
    than using a basic table. Other shaded tiles seems to be affected
    by each other.
    
    I think I have an idea on how to implement these shaded tiles
    this though.
    
    The same thing goes with the colored tiles.

--- src/Data/Mission/Til/Colorizer.h
+++ src/Data/Mission/Til/Colorizer.h
@@ -19,16 +19,6 @@ namespace Colorizer {
     };
 
     /**
-     * This function loads the mesh builder file. It configures create tile to use the json tile set.
-     * I do not normally practice globals, but this solution is only temporay.
-     * TODO eventually turn this script into c++ code too.
-     * @param filepath the path to this json file.
-     * @param error_output If there is a problem the class will put into this. If null then the class will say nothing.
-     * @return 1 for success, 0 for incompatibilties and -1 for not found.
-     */
-    unsigned int loadShadowScript( const char *const filepath, std::ostream *error_output );
-
-    /**
      * Set the colors for the square.
      * @param input This is where the inputs go.
      * @param result This is where the colors will be placed to. ALWAYS make sure that result refers to 4 vectors.
