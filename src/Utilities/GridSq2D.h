67608fed8f5ec0f3c1dabf7f7654dc969020a34a
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-08-25 14:22:35 -0700

    This is the new image system.
    * Grid2D is an abstract class that would contain a grid system.
    * GridReg2D stores the slots of the grid in a normal grid to row
    scheme.
    * GridSq2D stores slots that is optimized for the CPU cache. One
    limitation of this algorithm is that the height and width to be
    power by two, and for the grid to have the same width and height.
    * PixelFormat holds color information representations. It can
    handle converting between different color formats. I finally
    added 15 bit color support.
    * Image2D combines all the improvements of the PixelFormat and
    the GridReg2D, and makes an image.
    
    I need to make automated tests before using all these new classes.

---
+++ src/Utilities/GridSq2D.h
@@ -0,0 +1,140 @@
+#ifndef UTILITIES_GRID_SQ_2D_HEADER
+#define UTILITIES_GRID_SQ_2D_HEADER
+
+#include "Grid2D.h"
+
+namespace Utilities {
+
+//#if sizeof( grid_2d_unit ) != 2
+//#error Adjust the getOrderUnit method of ImageSq2D otherwise it will crash.
+//#endif
+
+typedef uint_fast32_t order_unit;
+
+// This should at least be twice the bytes as grid_2d_unit. Otherwise the algorithm is broken.
+//#if sizeof( order_unit ) >= sizeof( grid_2d_unit ) * 2
+//#error The Order Unit should be twice the bits as the grid_2d_unit.
+//#endif
+
+/**
+ * This class stores image data with a more cache optimized setup.
+ *
+ * The algorithm implemented is a Morton Curve. A morton curve stores cells or pixels, so they are stored
+ * closer to each other. This algorithm is named after Guy Macdonald Morton who invented this concept in 1966.
+ * I know it has another name, but due to current events I name this Morton Curve.
+ */
+template <class image_2d_pixel>
+class GridSq2D : public Grid2D<image_2d_pixel> {
+protected:
+    /**
+     * This method gets the m order unit from the cordinates.
+     * @param pos_x
+     * @param pos_y
+     * @return The m order value for the ordered curve.
+     */
+    static order_unit getOrderUnit( grid_2d_unit pos_x, grid_2d_unit pos_y ) {
+        // This algorithm is based on
+        // https://stackoverflow.com/questions/12157685/z-order-curve-coordinates
+        
+        static const order_unit MASKS[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+        
+        order_unit x = pos_x;
+        order_unit y = pos_y;
+        
+        x = (x | (x << 8)) & MASKS[3];
+        x = (x | (x << 4)) & MASKS[2];
+        x = (x | (x << 2)) & MASKS[1];
+        x = (x | (x << 1)) & MASKS[0];
+        
+        y = (y | (y << 8)) & MASKS[3];
+        y = (y | (y << 4)) & MASKS[2];
+        y = (y | (y << 2)) & MASKS[1];
+        y = (y | (y << 1)) & MASKS[0];
+        
+        return static_cast<order_unit>(x | (y << 1));
+    }
+public:
+    GridSq2D() : Grid2D<image_2d_pixel>() {}
+    GridSq2D( const GridSq2D& obj ) : Grid2D<image_2d_pixel>( obj ) {}
+    GridSq2D( grid_2d_unit width_param, grid_2d_unit height_param ) : Grid2D<image_2d_pixel>( width_param, height_param ) {}
+    
+    static bool isPowerOfTwo( grid_2d_unit dimension ) {
+        // This is based on this
+        // http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2.
+        // Otherwise it would not be a power of two image.
+        
+        if( dimension == 0 )
+            return false;
+        else
+            return ((dimension & (dimension - 1)) == 0);
+    }
+    
+    bool isValid() {
+        return isPowerOfTwo( this->width ) & isPowerOfTwo( this->height );
+    }
+    
+    inline void setInlinePixel( grid_2d_unit x, grid_2d_unit y, image_2d_pixel pixel ) {
+        if( x < this->width && y < this->height )
+        {
+            image_2d_pixel *destination_pixel = this->cells.data() +  getOrderUnit(x, y);
+            *destination_pixel = pixel;
+        }
+    }
+    
+    inline image_2d_pixel getInlinePixel( grid_2d_unit x, grid_2d_unit y ) const {
+        if( x < this->width && y < this->height )
+        {
+            return this->cells.at( getOrderUnit(x, y) );
+        }
+        else
+            return this->cells.at( 0 );
+    }
+    
+    virtual bool getCordinates( grid_2d_offset offset, grid_2d_unit &x, grid_2d_unit &y ) const {
+        x = 0;
+        y = 0;
+        
+        for( int i = 0; i < (sizeof(offset) / 2) * 8; i++ )
+        {
+            x |= ((offset >> (2 * i + 0)) & 1) << i;
+            y |= ((offset >> (2 * i + 1)) & 1) << i;
+        }
+        
+        return true;
+    }
+    
+    virtual grid_2d_offset getOffset( grid_2d_unit x, grid_2d_unit y ) const {
+        return getOrderUnit(x, y);
+    }
+    
+    /**
+     * This method only writes the pixel when it is in bounds.
+     * @param x the x location bounded by width.
+     * @param y the y location bounded by height.
+     * @return True if the pixel is written, false if no pixel is written.
+     */
+    virtual void setPixel( grid_2d_unit x, grid_2d_unit y, image_2d_pixel pixel ) {
+        setInlinePixel( x, y, pixel );
+    }
+
+    virtual const image_2d_pixel getPixel( grid_2d_unit x, grid_2d_unit y ) const {
+        return getInlinePixel( x, y );
+    }
+    
+    /**
+     * This method is to get the pixel that is used.
+     */
+    virtual const image_2d_pixel* getPixelRef( grid_2d_unit x, grid_2d_unit y ) const {
+        return this->cells.data() + getOrderUnit( x, y );
+    }
+    
+    virtual image_2d_pixel* getPixelRef( grid_2d_unit x, grid_2d_unit y ) {
+        return this->cells.data() + getOrderUnit( x, y );
+    }
+    
+};
+
+};
+
+#endif // UTILITIES_GRID_SQ_2D_HEADER
+
---
+++ src/Utilities/PixelFormat.h
@@ -0,0 +1,223 @@
+#ifndef UTILITIES_PIXEL_FORMAT_HEADER
+#define UTILITIES_PIXEL_FORMAT_HEADER
+
+#include "Buffer.h"
+
+#include <stdint.h>
+#include <vector>
+
+namespace Utilities {
+
+    typedef double channel_fp;
+    typedef uint16_t palette_index;
+
+    class PixelFormat {
+    public:
+        virtual ~PixelFormat() {}
+        virtual PixelFormat* duplicate() const = 0;
+    };
+
+    class PixelFormatByte : public PixelFormat {
+    public:
+        virtual uint_fast8_t byteSize() const = 0;
+    };
+
+    class PixelFormatBit : public PixelFormat {
+    public:
+        virtual uint_fast8_t bitSize() const = 0;
+    };
+
+    class PixelFormatColor : public PixelFormatByte {
+    public:
+        /**
+         * GenericColor is to always use linear color space.
+         */
+        struct GenericColor {
+            channel_fp red;
+            channel_fp green;
+            channel_fp blue;
+            channel_fp alpha;
+            
+            GenericColor() {}
+            GenericColor( channel_fp r, channel_fp g, channel_fp b, channel_fp a) : red( r ), green( g ), blue( b ), alpha( a ) {}
+            
+            /**
+             * This sets the grey scale value.
+             */
+            void setValue( channel_fp value ) {
+                red = value;
+                green = value;
+                blue = value;
+            }
+        };
+        enum ChannelInterpolation {
+            LINEAR,
+            sRGB
+        };
+        ChannelInterpolation interpolation;
+        
+        PixelFormatColor() : interpolation(LINEAR) {}
+        PixelFormatColor( ChannelInterpolation interpolate ) : interpolation(interpolate) {}
+        
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const GenericColor& color ) = 0;
+        virtual GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP ) = 0;
+    };
+
+    class PixelFormatColor_W8 : public PixelFormatColor {
+    public:
+        struct Color {
+            uint8_t white;
+            
+            Color() {}
+            Color( Utilities::PixelFormatColor::GenericColor generic, ChannelInterpolation interpolate );
+            
+            PixelFormatColor::GenericColor toGeneric( ChannelInterpolation interpolate ) const;
+        };
+        PixelFormatColor_W8() : PixelFormatColor(LINEAR) {}
+        PixelFormatColor_W8( ChannelInterpolation color ) : PixelFormatColor(color) {}
+        
+        virtual PixelFormat* duplicate() const {
+            return new PixelFormatColor_W8( interpolation );
+        }
+        virtual uint_fast8_t byteSize() const { return 1; }
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const PixelFormatColor::GenericColor& color );
+        virtual PixelFormatColor::GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+    };
+
+    class PixelFormatColor_W8A8 : public PixelFormatColor {
+    public:
+        struct Color {
+            uint8_t white;
+            uint8_t alpha;
+            
+            Color() {}
+            Color( Utilities::PixelFormatColor::GenericColor generic, ChannelInterpolation interpolate );
+            
+            PixelFormatColor::GenericColor toGeneric( ChannelInterpolation interpolate ) const;
+        };
+        PixelFormatColor_W8A8() : PixelFormatColor(LINEAR) {}
+        PixelFormatColor_W8A8( ChannelInterpolation color ) : PixelFormatColor(color) {}
+        
+        virtual PixelFormat* duplicate() const {
+            return new PixelFormatColor_W8A8( interpolation );
+        }
+        virtual uint_fast8_t byteSize() const { return 2; }
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const PixelFormatColor::GenericColor& color );
+        virtual PixelFormatColor::GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+    };
+
+    class PixelFormatColor_R5G5B5A1 : public PixelFormatColor {
+    public:
+        struct Color {
+            uint16_t red   : 5;
+            uint16_t green : 5;
+            uint16_t blue  : 5;
+            uint16_t alpha : 1;
+            
+            Color() {}
+            Color( Utilities::PixelFormatColor::GenericColor generic, ChannelInterpolation interpolate );
+            
+            PixelFormatColor::GenericColor toGeneric( ChannelInterpolation interpolate ) const;
+        };
+        PixelFormatColor_R5G5B5A1() : PixelFormatColor(LINEAR) {}
+        PixelFormatColor_R5G5B5A1( ChannelInterpolation color ) : PixelFormatColor(color) {}
+        
+        virtual PixelFormat* duplicate() const {
+            return new PixelFormatColor_R5G5B5A1( interpolation );
+        }
+        virtual uint_fast8_t byteSize() const { return 2; }
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const PixelFormatColor::GenericColor& color );
+        virtual PixelFormatColor::GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+    };
+
+    class PixelFormatColor_R8G8B8 : public PixelFormatColor {
+    public:
+        struct Color {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+            
+            Color() {}
+            Color( Utilities::PixelFormatColor::GenericColor generic, ChannelInterpolation interpolate );
+            
+            PixelFormatColor::GenericColor toGeneric( ChannelInterpolation interpolate ) const;
+        };
+        PixelFormatColor_R8G8B8() : PixelFormatColor(LINEAR) {}
+        PixelFormatColor_R8G8B8( ChannelInterpolation color ) : PixelFormatColor(color) {}
+        
+        virtual PixelFormat* duplicate() const {
+            return new PixelFormatColor_R8G8B8( interpolation );
+        }
+        virtual uint_fast8_t byteSize() const { return 3; }
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const PixelFormatColor::GenericColor& color );
+        virtual PixelFormatColor::GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+    };
+
+    class PixelFormatColor_R8G8B8A8 : public PixelFormatColor {
+    public:
+        struct Color {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+            uint8_t alpha;
+            
+            Color() {}
+            Color( Utilities::PixelFormatColor::GenericColor generic, ChannelInterpolation interpolate );
+            
+            PixelFormatColor::GenericColor toGeneric( ChannelInterpolation interpolate ) const;
+        };
+        PixelFormatColor_R8G8B8A8() : PixelFormatColor(LINEAR) {}
+        PixelFormatColor_R8G8B8A8( ChannelInterpolation color ) : PixelFormatColor(color) {}
+        
+        virtual PixelFormat* duplicate() const {
+            return new PixelFormatColor_R8G8B8A8( interpolation );
+        }
+        virtual uint_fast8_t byteSize() const { return 4; }
+        virtual void writePixel( Buffer::Writer &buffer, Buffer::Endian endian, const PixelFormatColor::GenericColor& color );
+        virtual PixelFormatColor::GenericColor readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+    };
+
+    class ColorPalette {
+    private:
+        PixelFormatColor *color_p;
+        Buffer buffer;
+        Buffer::Endian endianess;
+    public:
+        ColorPalette( const PixelFormatColor& color_palette, Buffer::Endian endianess = Buffer::Endian::NO_SWAP );
+        virtual ~ColorPalette() {
+            delete color_p;
+        }
+        
+        const PixelFormatColor& getColorFormat() const { return *color_p; }
+        
+        uint_fast16_t getTotalIndex() const;
+        
+        PixelFormatColor::GenericColor getIndex( palette_index index ) const;
+        bool setIndex( palette_index index, const PixelFormatColor::GenericColor color );
+    };
+
+    class PixelFormatIndex : public PixelFormatByte {
+    protected:
+        uint_fast8_t index_size;
+    public:
+        PixelFormatIndex( uint_fast8_t index_size_param );
+        
+        void writePixel( palette_index indexValue, Buffer::Writer &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+        palette_index readPixel( Buffer::Reader &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP );
+        virtual uint_fast8_t byteSize() const { return index_size; }
+    };
+
+    class PixelFormatBitIndex : public PixelFormatBit {
+    protected:
+        uint_fast8_t bits_per_index;
+    public:
+        PixelFormatBitIndex( uint_fast8_t bits_per_index_param );
+        
+        void writePixel( uint8_t *buffer_r, uint_fast8_t shift, palette_index indexValue );
+        palette_index readPixel( Buffer::Reader &buffer, uint_fast8_t offset, Buffer::Endian endian );
+        virtual uint_fast8_t bitSize() const { return bits_per_index; }
+    };
+}
+
+#endif // UTILITIES_PIXEL_FORMAT_HEADER
+
