be3dfb38e2b326f2209b683d79f54ed85c476d2e
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-08-26 11:35:40 -0700

    16 bit color now can be tested.

--- src/Tests/Utilities/PixelFormat.cpp
+++ src/Tests/Utilities/PixelFormat.cpp
@@ -83,7 +83,34 @@ int testColorProfiles( Utilities::PixelFormatColor::ChannelInterpolation interpo
             }
         }
     }
-    Utilities::PixelFormatColor_R5G5B5A1 r5g5b5a1( interpolate );
+    
+    {
+        Utilities::PixelFormatColor_R5G5B5A1 r5g5b5a1( interpolate );
+        Utilities::PixelFormatColor_R5G5B5A1::Color color[2];
+        
+        for( uint16_t v = 0; v <= 255; v++ )
+        {
+            color[0].red = v;
+            color[0].green = 15;
+            color[0].blue  = 3;
+            color[0].alpha = 0;
+            
+            generic = color[0].toGeneric( interpolate );
+            color[1] = Utilities::PixelFormatColor_R5G5B5A1::Color( generic, interpolate );
+            
+            if( color[0].red != color[1].red || color[0].green != color[1].green || color[0].blue != color[1].blue || color[0].alpha != color[1].alpha )
+            {
+                std::cout << "The color conversion for PixelFormatColor_R5G5B5A1 test has failed!" << std::endl;
+                std::cout << "  Color[0] changes on red." << std::endl;
+                std::cout << "  Color[0] " << static_cast<uint32_t>(color[0].red) << ", " << static_cast<uint32_t>(color[0].green) << ", " << static_cast<uint32_t>(color[0].blue) << ", " << static_cast<uint32_t>(color[0].alpha) << std::endl;
+                std::cout << "  Color[1] " << static_cast<uint32_t>(color[1].red) << ", " << static_cast<uint32_t>(color[1].green) << ", " << static_cast<uint32_t>(color[1].blue) << ", " << static_cast<uint32_t>(color[1].alpha) << std::endl;
+                printGeneric( generic );
+                v = 256;
+                problem = 1;
+            }
+        }
+    }
+    
     Utilities::PixelFormatColor_R8G8B8   r8g8b8( interpolate );
     Utilities::PixelFormatColor_R8G8B8A8 r8g8b8a8( interpolate );
     
