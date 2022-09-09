3b01fc540a5b66987a6cca2e129dbd3453c7fbcd
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-09-09 10:59:05 -0700

    For some reason the pixel format is bugged.
    This is the test the reviled the flaw.
    
    I need to find where the bug is though.

--- src/Tests/Utilities/Image2D.cpp
+++ src/Tests/Utilities/Image2D.cpp
@@ -1,15 +1,53 @@
 #include "../../Utilities/Image2D.h"
+#include <iostream>
 
 int main() {
-    // This checks if the grid 2d base functions.
     int problem = 0;
     
     // *** Image2D Test here.
     
-    // Test the declarations of the normal placemetn Image2D.
+    // Test the declarations of the normal placement Image2D.
     {Utilities::Image2D dec_test;}
-    {Utilities::Image2D dec_test( Utilities::Buffer::Endian::SWAP);}
-    {Utilities::Image2D dec_test(16,16, Utilities::PixelFormatColor_R5G5B5A1());}
+    {
+        Utilities::Image2D dec_test_0( Utilities::Buffer::Endian::SWAP );
+        
+        // Test the only parameter of the constructor.
+        if( dec_test_0.getEndian() != Utilities::Buffer::Endian::SWAP )
+        {
+            problem = 1;
+            std::cout << "Image2D( Utilities::Buffer::Endian::SWAP ) did not set the endianess!" << std::endl;
+        }
+        else
+        {
+            Utilities::Image2D dec_test_1( Utilities::Buffer::Endian::NO_SWAP );
+            
+            // Test the endianess
+            if( dec_test_1.getEndian() != Utilities::Buffer::Endian::NO_SWAP )
+            {
+                problem = 1;
+                std::cout << "Image2D( Utilities::Buffer::Endian::NO_SWAP ) did not set the endianess!" << std::endl;
+            }
+        }
+    }
+    {
+        Utilities::Image2D dec_test(16,16, Utilities::PixelFormatColor_R5G5B5A1());
+        
+        if( dynamic_cast<const Utilities::PixelFormatColor_R5G5B5A1*>( dec_test.getPixelFormat() ) != nullptr )
+        {
+            problem = 1;
+            std::cout << "Image2D( x, y, Utilities::PixelFormatColor_R5G5B5A1() ) did not set the pixel format!" << std::endl;
+        }
+        if( dec_test.getWidth() != 16 )
+        {
+            problem = 1;
+            std::cout << "Image2D( x, y, Utilities::PixelFormatColor_R5G5B5A1() ) did not set the width!" << std::endl;
+        }
+        if( dec_test.getHeight() != 16 )
+        {
+            problem = 1;
+            std::cout << "Image2D( x, y, Utilities::PixelFormatColor_R5G5B5A1() ) did not set the height!" << std::endl;
+        }
+    }
     {Utilities::Image2D dec_test(16,16, Utilities::PixelFormatColor_R8G8B8(),  Utilities::Buffer::Endian::SWAP);}
     {
         Utilities::Image2D dec_confirmed(16,16, Utilities::PixelFormatColor_R8G8B8());
@@ -17,9 +55,11 @@ int main() {
         Utilities::Image2D dec_test_1( dec_confirmed,  Utilities::PixelFormatColor_R5G5B5A1() );
     }
     
+    // 
+    
     // *** ImageMorbin2D Test here.
     
-    // Test the morbin code here
+    // Test the declarations of the morbin placement Image2D.
     {Utilities::ImageMorbin2D dec_test;}
     {Utilities::ImageMorbin2D dec_test( Utilities::Buffer::Endian::SWAP);}
     {Utilities::ImageMorbin2D dec_test(16,16, Utilities::PixelFormatColor_R5G5B5A1());}
