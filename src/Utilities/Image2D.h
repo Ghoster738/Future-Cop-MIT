a55636db888e5a04abb9de43e911dc3acbf332e4
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-09-17 11:04:34 -0700

    I have restructured the code.
    I am getting ready to make this color palette image class.

--- src/Utilities/Image2D.h
+++ src/Utilities/Image2D.h
@@ -13,33 +13,9 @@ typedef grid_2d_unit_strict image_strict_unit;
 template<class placement, class grid_2d_value = uint8_t>
 class ImageBase2D : protected GridBase2D<grid_2d_value, placement> {
 protected:
-    PixelFormatColor *pixel_format_p;
-    Buffer::Endian endian;
-    
-    virtual void updateCellBuffer() {
-        this->cells.resize(
-            static_cast<uint32_t>( this->getWidth() ) *
-            static_cast<uint32_t>( this->getHeight()) *
-            static_cast<uint32_t>( this->pixel_format_p->byteSize() ) );
-        this->placement.updatePlacement();
-    }
 public:
-    ImageBase2D( Buffer::Endian endian ) : ImageBase2D(0, 0, PixelFormatColor_R8G8B8(), endian ) {}
-    ImageBase2D( grid_2d_unit width, grid_2d_unit height, const PixelFormatColor& format, Buffer::Endian endian_param ) :
-        GridBase2D<grid_2d_value, placement>( width, height ),
-        pixel_format_p( nullptr ),
-        endian( endian_param )
-    {
-        // This casting is not dangerous as only the PixelFormatColor is being duplicated.
-        pixel_format_p = dynamic_cast<PixelFormatColor*>( format.duplicate() );
-        updateCellBuffer();
-    }
-    
-    virtual ~ImageBase2D() {
-        if( pixel_format_p != nullptr)
-            delete pixel_format_p;
-        pixel_format_p = nullptr;
-    }
+    ImageBase2D( grid_2d_unit width, grid_2d_unit height ) : GridBase2D<grid_2d_value, placement>( width, height ) {}
+    virtual ~ImageBase2D() {}
     
     /**
      * @return the grid's width.
@@ -54,12 +30,79 @@ public:
     /**
      * @return the endianess
      */
-    Buffer::Endian getEndian() const { return endian; }
+    virtual Buffer::Endian getEndian() const = 0;
     
     /**
      * @return the pixel format.
      */
-    const PixelFormatColor *const getPixelFormat() const { return pixel_format_p; }
+    virtual const PixelFormatColor *const getPixelFormat() const = 0;
+    
+    virtual PixelFormatColor::GenericColor readPixel( grid_2d_unit x, grid_2d_unit y ) const = 0;
+    
+    virtual bool inscribeSubImage( grid_2d_unit x, grid_2d_unit y, const ImageBase2D<placement>& ref ) = 0;
+
+    virtual bool subImage( grid_2d_unit x, grid_2d_unit y, grid_2d_unit width, grid_2d_unit height, ImageBase2D<placement> &sub_grid ) const = 0;
+    
+    /**
+     * Flip the image vertically.It is an O(p) operation, and p is the
+     * number pixels.
+     */
+    virtual void flipHorizontally() = 0;
+    
+    /**
+     * Flip the image horizontally.It is an O(p) operation, and p is the
+     * number pixels.
+     */
+    virtual void flipVertically() = 0;
+    
+    virtual bool fromReader( Buffer::Reader &reader, Buffer::Endian endian ) = 0;
+    
+    virtual bool toWriter( Buffer::Writer &writer, Buffer::Endian endian ) const = 0;
+    
+    virtual bool addToBuffer( Buffer &buffer, Buffer::Endian endian ) const = 0;
+};
+
+template<class placement, class grid_2d_value = uint8_t>
+class ImageBaseWrite2D : public ImageBase2D<placement, grid_2d_value> {
+protected:
+    PixelFormatColor *pixel_format_p;
+    Buffer::Endian endian;
+    
+    virtual void updateCellBuffer() {
+        this->cells.resize(
+            static_cast<uint32_t>( this->getWidth() ) *
+            static_cast<uint32_t>( this->getHeight()) *
+            static_cast<uint32_t>( this->pixel_format_p->byteSize() ) );
+        this->placement.updatePlacement();
+    }
+public:
+    ImageBaseWrite2D( Buffer::Endian endian = Buffer::Endian::NO_SWAP ) :
+        ImageBaseWrite2D(0, 0, PixelFormatColor_R8G8B8(), endian ) {}
+    ImageBaseWrite2D( grid_2d_unit width, grid_2d_unit height, const PixelFormatColor& format, Buffer::Endian endian_param = Buffer::Endian::NO_SWAP  ) :
+        ImageBase2D<placement, grid_2d_value>( width, height ), endian( endian_param ) {
+        pixel_format_p = dynamic_cast<PixelFormatColor*>( format.duplicate() );
+        updateCellBuffer();
+    }
+    
+    virtual ~ImageBaseWrite2D() {
+        if( pixel_format_p != nullptr )
+            delete pixel_format_p;
+        pixel_format_p = nullptr;
+    }
+    
+    /**
+     * @return the endianess
+     */
+    virtual Buffer::Endian getEndian() const {
+        return endian;
+    }
+    
+    /**
+     * @return the pixel format.
+     */
+    virtual const PixelFormatColor *const getPixelFormat() const {
+        return pixel_format_p;
+    }
     
     virtual void setValue( grid_2d_unit x, grid_2d_unit y, grid_2d_value pixel ) {
         if( !this->size.withinBounds(x, y) )
@@ -90,7 +133,7 @@ public:
      * This method is to get the pixel that is used.
      */
     virtual const grid_2d_value* getRef( grid_2d_unit x, grid_2d_unit y ) const {
-        return const_cast<ImageBase2D<placement,grid_2d_value>*>(this)->getRef( x, y );
+        return const_cast<ImageBaseWrite2D<placement,grid_2d_value>*>(this)->getRef( x, y );
     }
     
     virtual grid_2d_value* getRef( grid_2d_unit x, grid_2d_unit y ) {
@@ -100,48 +143,12 @@ public:
         else
         {
             const size_t OFFSET = this->placement.getOffset(x, y) * static_cast<size_t>( pixel_format_p->byteSize() );
-                                   
             
             assert( OFFSET < this->cells.size() );
             
             return this->cells.data() + OFFSET;
         }
     }
-    
-    virtual PixelFormatColor::GenericColor readPixel( grid_2d_unit x, grid_2d_unit y ) const = 0;
-    
-    virtual bool inscribeSubImage( grid_2d_unit x, grid_2d_unit y, const ImageBase2D<placement>& ref ) = 0;
-
-    virtual bool subImage( grid_2d_unit x, grid_2d_unit y, grid_2d_unit width, grid_2d_unit height, ImageBase2D<placement> &sub_grid ) const = 0;
-    
-    /**
-     * Flip the image vertically.It is an O(p) operation, and p is the
-     * number pixels.
-     */
-    virtual void flipHorizontally() = 0;
-    
-    /**
-     * Flip the image horizontally.It is an O(p) operation, and p is the
-     * number pixels.
-     */
-    virtual void flipVertically() = 0;
-    
-    virtual bool fromReader( Buffer::Reader &reader, Buffer::Endian endian ) = 0;
-    
-    virtual bool toWriter( Buffer::Writer &writer, Buffer::Endian endian ) const = 0;
-    
-    virtual bool addToBuffer( Buffer &buffer, Buffer::Endian endian ) const = 0;
-};
-
-template<class placement, class grid_2d_value = uint8_t>
-class ImageBaseWrite2D : public ImageBase2D<placement, grid_2d_value> {
-public:
-    ImageBaseWrite2D( Buffer::Endian endian = Buffer::Endian::NO_SWAP ) :
-        ImageBase2D<placement, grid_2d_value>( endian ) {}
-    ImageBaseWrite2D( grid_2d_unit width, grid_2d_unit height, const PixelFormatColor& format, Buffer::Endian endian = Buffer::Endian::NO_SWAP  ) :
-        ImageBase2D<placement, grid_2d_value>(width, height, format, endian ) {}
-    
-    virtual ~ImageBaseWrite2D() {}
     
     virtual bool writePixel( grid_2d_unit x, grid_2d_unit y, PixelFormatColor::GenericColor color ) = 0;
 };
