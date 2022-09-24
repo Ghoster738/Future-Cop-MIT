71edc3300dbf8deeb680f3216e7b5627c0c30c1b
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-09-24 12:23:03 -0700

    Finally, creating a test for ImagePalette2D.
    I have also seperated code from Image2D.
    This is to make code eaiser to read.

--- src/Utilities/Image2D.h
+++ src/Utilities/Image2D.h
@@ -215,56 +215,6 @@ public:
     bool addToBuffer( Buffer &buffer, Buffer::Endian endian = Buffer::Endian::NO_SWAP ) const;
 };
 
-template<class placement, class grid_2d_value = uint8_t>
-class ImagePalette2D : public ImageBase2D<placement, grid_2d_value> {
-protected:
-    ColorPalette *color_palette_p;
-    
-public:
-    ImagePalette2D( const ColorPalette& palette ) :
-        ImagePalette2D(0, 0, palette ) {}
-    ImagePalette2D( grid_2d_unit width, grid_2d_unit height, const ColorPalette& palette  ) :
-        ImageBase2D<placement, grid_2d_value>( width, height ) {
-        color_palette_p = new ColorPalette( palette );
-    }
-    
-    virtual ~ImagePalette2D() {
-        if( color_palette_p != nullptr )
-            delete color_palette_p;
-        color_palette_p = nullptr;
-    }
-    
-    /**
-     * @return the endianess
-     */
-    virtual Buffer::Endian getEndian() const {
-        return color_palette_p->getEndian();
-    }
-    
-    /**
-     * @return the pixel format.
-     */
-    virtual const PixelFormatColor *const getPixelFormat() const {
-        return &color_palette_p->getColorFormat();
-    }
-    
-    virtual const ColorPalette *const getColorPalette() const {
-        return color_palette_p;
-    }
-    
-    virtual bool inscribeSubImage( grid_2d_unit x, grid_2d_unit y, const ImagePalette2D<placement>& ref ) = 0;
-
-    virtual bool subImage( grid_2d_unit x, grid_2d_unit y, grid_2d_unit width, grid_2d_unit height, ImagePalette2D<placement> &sub_image ) const = 0;
-    
-    uint_fast8_t getPixelIndex( grid_2d_unit x, grid_2d_unit y ) {
-        return this->getValue( x, y );
-    }
-    
-    bool writePixel( grid_2d_unit x, grid_2d_unit y, uint_fast8_t index ) {
-        return this->setValue( x, y, index );
-    }
-};
-
 }
 
 #endif // UTILITIES_IMAGE_2D_HEADER
