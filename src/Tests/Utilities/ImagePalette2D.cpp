3624c68cbea5793e9a8ee3142c0d1fd36cc21719
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-09-30 23:46:23 -0700

    Created a color palette generator.

--- src/Tests/Utilities/ImagePalette2D.cpp
+++ src/Tests/Utilities/ImagePalette2D.cpp
@@ -4,6 +4,47 @@
 
 #include "TestImage2D.h"
 
+void transitionColor( std::vector<Utilities::PixelFormatColor::GenericColor> &generated, size_t offset ) {
+    Utilities::PixelFormatColor::GenericColor color[2];
+    Utilities::PixelFormatColor::GenericColor new_color;
+    
+    color[ 0 ] = generated.at( offset );
+    color[ 1 ] = generated.at( (offset + 64) % 0x100 );
+    
+    for( unsigned i = 1; i < 64; i++ )
+    {
+        float factor = ((float)i) / 64.0;
+        
+        new_color.red   = color[ 0 ].red   * (1.0f - factor) + color[1].red   * factor;
+        new_color.green = color[ 0 ].green * (1.0f - factor) + color[1].green * factor;
+        new_color.blue  = color[ 0 ].blue  * (1.0f - factor) + color[1].blue  * factor;
+        new_color.alpha = color[ 0 ].alpha * (1.0f - factor) + color[1].alpha * factor;
+        
+        generated[ i + offset ] = new_color;
+    }
+}
+
+std::vector<Utilities::PixelFormatColor::GenericColor> generateColorPalette()
+{
+    std::vector<Utilities::PixelFormatColor::GenericColor> generated( 0x100 );
+    
+    generated.at(   0 ) = Utilities::PixelFormatColor::GenericColor( 1, 0, 0, 1 );
+    generated.at(  64 ) = Utilities::PixelFormatColor::GenericColor( 0, 1, 0, 1 );
+    generated.at( 128 ) = Utilities::PixelFormatColor::GenericColor( 0, 1, 1, 1 );
+    generated.at( 192 ) = Utilities::PixelFormatColor::GenericColor( 0, 0, 1, 1 );
+    
+    transitionColor( generated, 0 );
+    transitionColor( generated, 64 );
+    transitionColor( generated, 128 );
+    transitionColor( generated, 192 );
+    
+    for( unsigned i = 0; i < 256; i++ ) {
+        std::cout << "P[" << i << "] = " << generated[ i ].getString() << std::endl;
+    }
+    
+    return generated;
+}
+
 bool checkColorPalette() {
     Utilities::PixelFormatColor_R8G8B8 color;
     Utilities::ColorPalette color_palette( color );
@@ -51,7 +92,9 @@ bool checkColorPalette() {
     }
     if( testColor( 0, color_palette.getIndex( 0 ), FIRST_COLOR, "Palette test", "" ) )
         return false;
+    generateColorPalette();
     
+    return false;
     return true;
 }
 
