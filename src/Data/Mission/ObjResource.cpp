c1510b7f3c16b1d269926be164490c8e16b84a86
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-11-09 02:40:36 -0800

    I cannot find what is wrong.

--- src/Data/Mission/ObjResource.cpp
+++ src/Data/Mission/ObjResource.cpp
@@ -999,19 +999,18 @@ Utilities::ModelBuilder * Data::Mission::ObjResource::createModel( const std::ve
 
         model_output->allocateJoints( bones.size(), bone_frames );
         
-        unsigned int childern[ max_bone_childern ];
         glm::mat4 bone_matrix;
         unsigned int opcode_decode = 0;
         
         const float ANGLE_UNITS_TO_RADIANS = M_PI / 2048.0;
         
         // Make joint relations.
-        for( unsigned int bone_index = 1; bone_index < bones.size(); bone_index++ ) {
-            auto child_index  = bone_index;
-            auto parent_index = bone_index - 1;
+        unsigned int childern[ max_bone_childern ];
+        for( unsigned int bone_index = 0; bone_index < bones.size(); bone_index++ ) {
+            childern[ bones.at( bone_index ).parent_amount - 1 ] = bone_index;
             
-            if( bones.at( child_index ).parent_amount != bones.at( parent_index ).parent_amount )
-                model_output->setJointParent( parent_index, child_index );
+            if( bones.at( bone_index ).parent_amount > 1 )
+                model_output->setJointParent( childern[ bones.at( bone_index ).parent_amount - 2 ], bone_index );
         }
 
         for( unsigned int bone_index = 0; bone_index < bones.size(); bone_index++ ) {
@@ -1041,7 +1040,7 @@ Utilities::ModelBuilder * Data::Mission::ObjResource::createModel( const std::ve
                 bone_matrix = glm::rotate( bone_matrix, static_cast<float>( frame_rotation.y ) * ANGLE_UNITS_TO_RADIANS, glm::vec3( 1, 0, 0 ) );
                 bone_matrix = glm::rotate( glm::mat4(1.0f), -static_cast<float>( frame_rotation.z ) * ANGLE_UNITS_TO_RADIANS, glm::vec3( 0, 0, 1 ) ) * bone_matrix;
                 
-                auto position  = glm::vec3( -frame_position.x, frame_position.y, frame_position.z ) * static_cast<float>( INTEGER_FACTOR );
+                auto position  = glm::vec3( -static_cast<float>( frame_position.x ) * INTEGER_FACTOR, static_cast<float>( frame_position.y ) * INTEGER_FACTOR, static_cast<float>( frame_position.z ) * INTEGER_FACTOR);
                 auto quaterion = glm::quat_cast( bone_matrix );
                 
                 model_output->setJointFrame( frame, bone_index, position, quaterion );
