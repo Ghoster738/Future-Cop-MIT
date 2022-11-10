9e362096e52524682652ccbf5c39280ef62acbc7
Ghoster <101094904+Ghoster738@users.noreply.github.com>
2022-11-09 16:57:56 -0800

    I added a discard case for the shaders.

--- src/Graphics/SDL2/GLES2/Internal/StaticModelDraw.cpp
+++ src/Graphics/SDL2/GLES2/Internal/StaticModelDraw.cpp
@@ -48,8 +48,8 @@ const GLchar* Graphics::SDL2::GLES2::Internal::StaticModelDraw::default_fragment
     "void main()\n"
     "{\n"
     "  vec4 color = texture2D(Texture, texture_coord_1);\n"
-    "  const float CUTOFF = 0.03125;\n"
-    "  if( color.r < CUTOFF && color.g < CUTOFF && color.b < CUTOFF )"
+    "  const float CUTOFF = 0.015625;\n"
+    "  if( color.r < CUTOFF && color.g < CUTOFF && color.b < CUTOFF && color.a == 1.0 )"
     "    discard;\n"
     "  gl_FragColor = texture2D(Shine, world_reflection.xz) * specular + color;\n"
     "}\n";
