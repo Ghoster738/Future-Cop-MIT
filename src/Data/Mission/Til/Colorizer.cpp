0ef03a129e92d73d2755c51e56e225dd84660203
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-01-02 11:49:25 -0800

    Adjusted the colors to have a certain alignment

--- src/Data/Mission/Til/Colorizer.cpp
+++ src/Data/Mission/Til/Colorizer.cpp
@@ -118,10 +118,22 @@ unsigned int Data::Mission::Til::Colorizer::setSquareColors( const Input &input,
                 }
                 break;
         }
-        for( unsigned int p = 0; p < 4; p++ )
-        {
-            result_r[ p ] = static_cast<glm::vec3>( input.position ) / static_cast<float>(Data::Mission::TilResource::AMOUNT_OF_TILES);
-        }
+        
+        result_r[0].x = 0.00;
+        result_r[0].y = 0.00;
+        result_r[0].z = 0.00;
+        
+        result_r[1].x = 0.00;
+        result_r[1].y = 1.00;
+        result_r[1].z = 0.00;
+        
+        result_r[2].x = 1.00;
+        result_r[2].y = 1.00;
+        result_r[2].z = 0.00;
+        
+        result_r[3].x = 1.00;
+        result_r[3].y = 0.00;
+        result_r[3].z = 0.00;
 
         return 1;
     }
