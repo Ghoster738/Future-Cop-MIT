d85c7cfee9e529af51db757ca33f1d9f38def764
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-01-23 16:05:16 -0800

    Fixed coloring method further.
    This coloring method works for the center Til for urban jungle,
    but the rest looks incorrect.

--- src/Data/Mission/Til/Colorizer.cpp
+++ src/Data/Mission/Til/Colorizer.cpp
@@ -86,70 +86,44 @@ unsigned int Data::Mission::Til::Colorizer::setSquareColors( const Input &input,
 {
     if( result_r != nullptr )
     {
-        glm::u8vec3 inverse_positions[3];
-        inverseSet( input.position, inverse_positions );
+        result_r[0] = getColorVec3( input.tile, input.colors );
         
-        const unsigned numb_x = 5;
-        const unsigned numb_y = 5;
-        
-        // if( input.position.x == numb_x && input.position.y == numb_y ) {
-            result_r[0] = getColorVec3( input.tile, input.colors );
-            
-            result_r[1].x = 1.0;
-            result_r[1].y = 0.0;
-            result_r[1].z = 0.0;
-            result_r[1] = colorToVec3( input.color_map.getColor( inverse_positions[ 0 ], input.colors ) );
-            
-            result_r[2].x = 1.0;
-            result_r[2].y = 1.0;
-            result_r[2].z = 0.0;
-            result_r[2] = colorToVec3( input.color_map.getColor( inverse_positions[ 1 ], input.colors ) );
-            
-            result_r[3].x = 0.0;
-            result_r[3].y = 1.0;
-            result_r[3].z = 0.0;
-            result_r[3] = colorToVec3( input.color_map.getColor( inverse_positions[ 2 ], input.colors ) );
-            
-            
-        /*}
-        else
-        if( input.position.x == numb_x && inverse( input.position.y ) == numb_y ) {
-            for( unsigned int p = 0; p < 4; p++ )
-            {
-                result_r[p].x = 1.0;
-                result_r[p].y = 0.0;
-                result_r[p].z = 0.0;
-            }
+        switch( input.tile.type ) {
+            case 0b00: // Solid Monochrome
+                {
+                    result_r[1] = result_r[0];
+                    result_r[2] = result_r[0];
+                    result_r[3] = result_r[0];
+                }
+                break;
+            case 0b01: // Dynamic Monochrome
+                {
+                    glm::u8vec3 inverse_positions[3];
+                    inverseSet( input.position, inverse_positions );
+                    
+                    result_r[1] = colorToVec3( input.color_map.getColor( inverse_positions[ 0 ], input.colors ) );
+                    result_r[2] = colorToVec3( input.color_map.getColor( inverse_positions[ 1 ], input.colors ) );
+                    result_r[3] = colorToVec3( input.color_map.getColor( inverse_positions[ 2 ], input.colors ) );
+                }
+                break;
+            case 0b10: // Dynamic Color
+                {
+                    glm::u8vec3 inverse_positions[3];
+                    inverseSet( input.position, inverse_positions );
+                    
+                    result_r[1] = colorToVec3( input.color_map.getColor( inverse_positions[ 0 ], input.colors ) );
+                    result_r[2] = colorToVec3( input.color_map.getColor( inverse_positions[ 1 ], input.colors ) );
+                    result_r[3] = colorToVec3( input.color_map.getColor( inverse_positions[ 2 ], input.colors ) );
+                }
+                break;
+            case 0b11: // Lava Animation
+                {
+                    result_r[1] = glm::vec3(1.0, 1.0, 1.0) - result_r[0];
+                    result_r[2] = result_r[1];
+                    result_r[3] = result_r[1];
+                }
+                break;
         }
-        else
-        if( inverse( input.position.x ) == numb_x && input.position.y == numb_y ) {
-            for( unsigned int p = 0; p < 4; p++ )
-            {
-                result_r[p].x = 0.0;
-                result_r[p].y = 1.0;
-                result_r[p].z = 0.0;
-            }
-        }
-        else
-        if( inverse( input.position.x ) == numb_x && inverse( input.position.y ) == numb_y ) {
-            for( unsigned int p = 0; p < 4; p++ )
-            {
-                result_r[p].x = 1.0;
-                result_r[p].y = 1.0;
-                result_r[p].z = 0.0;
-            }
-        }
-        else
-        {
-            result_r[0] = getColorVec3( input.tile, input.colors );
-            
-            for( unsigned int p = 1; p < 4; p++ )
-            {
-                result_r[p].x = result_r[0].x;
-                result_r[p].y = result_r[0].x;
-                result_r[p].z = result_r[0].x;
-            }
-        }*/
 
         return 1;
     }
