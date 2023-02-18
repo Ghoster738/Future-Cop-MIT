9f6649f1808109fefa1c2580b7846088db433ec8
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-02-17 17:20:22 -0800

    There is something wrong with the QOI status.
    Either read mode or write mode.

--- src/Tests/Utilities/ImageFormat/QuiteOkayImage.cpp
+++ src/Tests/Utilities/ImageFormat/QuiteOkayImage.cpp
@@ -1,6 +1,5 @@
 #include "../../../Utilities/Image2D.h"
 #include "../../../Utilities/ImageFormat/QuiteOkImage.h"
-#include "../../../Utilities/ImageFormat/PortableNetworkGraphics.h"
 
 #include <glm/vec2.hpp>
 #include <iostream>
@@ -27,6 +26,33 @@ const Utilities::Image2D generateFractalImage( const unsigned WIDTH, const unsig
     return source;
 }
 
+int defaultState( std::string name, Utilities::ImageFormat::QuiteOkImage::QOIStatus status ) {
+    int error_state = 0;
+    
+    if( status.used_RGB == 0 ) {
+        std::cout << name << ":RGB is not used!" << std::endl;
+        error_state = 1;
+    }
+    if( status.used_index == 0 ) {
+        std::cout << name << ":Index is not used!" << std::endl;
+        error_state = 1;
+    }
+    if( status.used_diff == 0 ) {
+        std::cout << name << ":Diff is not used!" << std::endl;
+        error_state = 1;
+    }
+    if( status.used_luma == 0 ) {
+        std::cout << name << ":Luma is not used!" << std::endl;
+        error_state = 1;
+    }
+    if( status.used_run == 0 ) {
+        std::cout << name << ":Run is not used!" << std::endl;
+        error_state = 1;
+    }
+    
+    return error_state;
+}
+
 int main() {
     int error_state = 0;
     
@@ -41,18 +67,22 @@ int main() {
         
         if( qoi_format.write( original, buffer ) != 1 ) {
             std::cout << name << ": has failed to write image to buffer" << std::endl;
-            error_state = -1;
+            error_state = 1;
         }
+        
+        error_state |= defaultState( name + " write mode", qoi_format.getStatus() );
         
         Utilities::Image2D read_image( 0, 0, *original.getPixelFormat() );
         
         if( qoi_format.read( buffer, read_image ) != 1 ) {
             std::cout << name << ": has failed to read image from buffer" << std::endl;
-            error_state = -1;
+            error_state = 1;
         }
         
+        error_state |= defaultState( name + " read mode", qoi_format.getStatus() );
+        
         if( testCopyOperator( original, read_image, WIDTH, HEIGHT, name ) ) {
-            error_state = -1;
+            error_state = 1;
         }
     }
     {
@@ -69,12 +99,16 @@ int main() {
             error_state = -1;
         }
         
+        error_state |= defaultState( name + " write mode", qoi_format.getStatus() );
+        
         Utilities::Image2D read_image( 0, 0, *original.getPixelFormat() );
         
         if( qoi_format.read( buffer, read_image ) != 1 ) {
             std::cout << name << ": has failed to read image from buffer" << std::endl;
             error_state = -1;
         }
+        
+        error_state |= defaultState( name + " read mode", qoi_format.getStatus() );
         
         if( testCopyOperator( original, read_image, WIDTH, HEIGHT, name ) ) {
             error_state = -1;
