d04df27eee16e4f845c132263e4a5a8a7f465b87
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-03-17 11:20:21 -0700

    The font reading system is now fully tested.
    The computer font is now checked on both platforms Windows, and
    Macintosh.
    
    This means that I can now modify the font system.
    
    The one draw back with this checking approach is that if the internal
    fonts gets modified then the unit tests would most likely fail.

--- src/Tests/Data/Mission/FontResource.cpp
+++ src/Tests/Data/Mission/FontResource.cpp
@@ -783,21 +783,22 @@ int checkImage( const Data::Mission::FontResource &font, size_t qoi_len,
 
 struct GlyphChecker {
     uint8_t glyphID;
+    glm::i8vec2 offset;
     uint8_t width;
     uint8_t height;
     uint8_t left;
     uint8_t top;
     uint8_t x_advance;
     
-    GlyphChecker() : glyphID( 0 ) {}
-    GlyphChecker( uint8_t glyphID_param ) : glyphID( glyphID_param ) {}
-    GlyphChecker( uint8_t glyphID_param, uint8_t width_param, uint8_t height_param, uint8_t left_param, uint8_t top_param, uint8_t x_advance_param ) : glyphID( glyphID_param ), width( width_param ), height( height_param ),left( left_param ), top( top_param ), x_advance( x_advance_param ) {}
+    GlyphChecker() : glyphID( 0 ), offset(0,0) {}
+    GlyphChecker( uint8_t glyphID_param ) : glyphID( glyphID_param ), offset(0,0) {}
+    GlyphChecker( uint8_t glyphID_param, uint8_t width_param, uint8_t height_param, uint8_t left_param, uint8_t top_param, uint8_t x_advance_param, int8_t offset_x = 0, int8_t offset_y = 0 ) : glyphID( glyphID_param ), offset(offset_x, offset_y), width( width_param ), height( height_param ),left( left_param ), top( top_param ), x_advance( x_advance_param ) {}
     
     int check( const Data::Mission::FontGlyph &glyph, std::string name ) const {
         int problem = 0;
         
         if( glyph.getGlyph() != glyphID ) {
-            std::cout << name << " has a hashing bug detected with " << (uint16_t)glyphID << "\n";
+            std::cout << name << " has a hashing bug detected with " << (uint16_t)glyphID << " which is '" << glyphID << "'\n";
             std::cout << (uint16_t)glyph.getGlyph() << " is placed in " << glyphID << std::endl;
             
             problem = 1;
@@ -827,6 +828,17 @@ struct GlyphChecker {
             
             problem = 1;
         }
+        if( glyph.getOffset() != offset )
+        {
+            std::cout << name << " has an offset with " << (uint16_t)glyphID << "\n";
+            std::cout << " (" << (int16_t)glyph.getOffset().x << ", " << (int16_t)glyph.getOffset().y << ") while it should be (" << (int16_t)offset.x << ", " << (int16_t)offset.y << ")" << std::endl;
+
+            problem = 1;
+        }
+
+        if( problem ) {
+            std::cout << "  charcode " << (uint16_t)glyphID << " = " << glyphID << std::endl;
+        }
         
         return problem;
     }
@@ -852,7 +864,7 @@ int checkFont( const Data::Mission::FontResource &font, const std::set<GlyphChec
                 if( accessor == expected_glyphs.end() ) {
                     character_problem = 1;
 
-                    std::cout << name << " has a glyph that is not in the test with " << i << std::endl;
+                    std::cout << name << " has a glyph that is not in the test with " << i << " or '" << (char)i << "'" << std::endl;
                 }
                 else if( (*accessor).check( *glyph_r, name ) == 1 )
                 {
@@ -871,11 +883,134 @@ int checkFont( const Data::Mission::FontResource &font, const std::set<GlyphChec
     return character_problem;
 }
 
+std::set<GlyphChecker> generateComputerFont() {
+    std::set<GlyphChecker> glyphs;
+
+    //                           code, width, height, left, top, x advance, of x, of y
+    glyphs.insert( GlyphChecker(  '0',     7,      8,    0,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '1',     3,      8,  248,   1,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  '2',     7,      8,    8,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '3',     7,      8,   16,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '4',     7,      8,   24,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '5',     7,      8,   32,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '6',     7,      8,   40,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '7',     7,      8,   48,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '8',     7,      8,   56,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '9',     7,      8,   64,   1,         8 ) );
+
+    glyphs.insert( GlyphChecker(  ' ',     1,      1,  254,  23,         8 ) );
+    glyphs.insert( GlyphChecker(  '@',     7,      8,   72,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '#',     7,      8,   80,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '$',     7,      8,   88,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '%',     7,      8,   96,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '&',     7,      8,  104,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  '*',     5,      5,  100,  19,         8,    1,    1 ) );
+    glyphs.insert( GlyphChecker(  '?',     7,      8,  200,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  ';',     2,      6,  181,  11,         8,    3,    1 ) );
+    glyphs.insert( GlyphChecker(  ':',     1,      5,  143,  19,         8,    3,    1 ) );
+    glyphs.insert( GlyphChecker(  ',',     2,      2,   97,  19,         8,    2,    6 ) );
+    glyphs.insert( GlyphChecker(  '.',     1,      1,   95,  19,         8,    3,    7 ) );
+    glyphs.insert( GlyphChecker(  '!',     1,      8,  242,   1,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  '-',     5,      1,  137,  21,         8,    1,    3 ) );
+    glyphs.insert( GlyphChecker(  '_',     7,      1,  123,  19,         8,    0,    7 ) );
+    glyphs.insert( GlyphChecker(  '+',     5,      5,  131,  19,         8,    1,    1 ) );
+    glyphs.insert( GlyphChecker(  '=',     7,      4,  224,  19,         8,    2,    2 ) );
+    glyphs.insert( GlyphChecker(  '^',     3,      2,  119,  19,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  '`',     3,      3,  177,  10,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  '~',     5,      3,  113,  19,         8,    2 ) );
+
+    glyphs.insert( GlyphChecker(  'a',     7,      5,  176,   4,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'b',     7,      8,  112,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  'c',     6,      5,  145,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'd',     7,      8,  120,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  'e',     7,      5,  152,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'f',     7,      8,  128,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  'g',     7,      8,  136,   1,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'h',     7,      8,  144,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  'i',     1,      8,  252,   1,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  'j',     3,      8,  244,   1,         8,    2,    1 ) );
+    glyphs.insert( GlyphChecker(  'k',     7,      8,  152,   1,         8 ) );
+    glyphs.insert( GlyphChecker(  'l',     2,      8,  239,   1,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  'm',     7,      5,  160,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'n',     7,      5,  168,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'o',     7,      5,  184,   4,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'p',     7,      8,  160,   1,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'q',     7,      8,  168,   1,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'r',     7,      5,  176,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  's',     7,      5,  184,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  't',     4,      8,  231,   1,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  'u',     7,      5,  192,   4,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'v',     7,      5,  192,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'w',     7,      5,  200,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'x',     7,      5,  208,  19,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'y',     7,      8,  161,  10,         8,    0,    3 ) );
+    glyphs.insert( GlyphChecker(  'z',     7,      5,  216,  19,         8,    0,    3 ) );
+
+    glyphs.insert( GlyphChecker( '\'',     1,      2,  111,  19,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  '"',     3,      2,  106,  19,         8,    2 ) );
+
+    glyphs.insert( GlyphChecker(  'A',     7,      8,  232,  13,         8 ) );
+    uint8_t offset = 1;
+    for( uint8_t c = 'A' + 1; c < 'I'; c++ ) {
+        glyphs.insert( GlyphChecker(  c,   7,      8, offset, 10,         8 ) );
+        offset += 8;
+    }
+    glyphs.insert( GlyphChecker(  'I',     3,      8,  184,  10,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  'J',     5,      8,  188,  10,         8,    1 ) );
+    glyphs.insert( GlyphChecker(  'K',     7,      8,   57,  10,         8 ) );
+    glyphs.insert( GlyphChecker(  'L',     5,      8,  194,  10,         8,    1 ) );
+    glyphs.insert( GlyphChecker(  'M',     7,      8,   65,  10,         8 ) );
+    glyphs.insert( GlyphChecker(  'N',     7,      8,   73,  10,         8 ) );
+    glyphs.insert( GlyphChecker(  'O',     7,      8,  240,  13,         8 ) );
+    offset = 81;
+    for( uint8_t c = 'P'; c < 'U'; c++ ) {
+        glyphs.insert( GlyphChecker(  c,   7,      8, offset, 10,         8 ) );
+        offset += 8;
+    }
+    glyphs.insert( GlyphChecker(  'U',     7,      8,  248,  13,         8 ) );
+    offset = 121;
+    for( uint8_t c = 'U' + 1; c < 'Z' + 1; c++ ) {
+        glyphs.insert( GlyphChecker(  c,   7,      8, offset, 10,         8 ) );
+        offset += 8;
+    }
+
+    glyphs.insert( GlyphChecker( '\\',     7,      7,  200,  11,         8 ) );
+    glyphs.insert( GlyphChecker(  '|',     1,      8,  236,   1,         8,    3 ) );
+    glyphs.insert( GlyphChecker(  '/',     7,      7,  224,  11,         8 ) );
+
+    glyphs.insert( GlyphChecker(  '<',     7,      7,  208,  11,         8 ) );
+    glyphs.insert( GlyphChecker(  '>',     7,      7,  216,  11,         8 ) );
+
+    glyphs.insert( GlyphChecker(  '(',     3,      8,  223,   1,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  ')',     3,      8,  227,   1,         8,    2 ) );
+
+    glyphs.insert( GlyphChecker(  '[',     2,      8,  209,   1,         8,    2 ) );
+    glyphs.insert( GlyphChecker(  ']',     2,      8,  212,   1,         8,    2 ) );
+
+    glyphs.insert( GlyphChecker(  '{',     3,      7,  215,   1,         8,    1,    1 ) );
+    glyphs.insert( GlyphChecker(  '}',     3,      7,  219,   1,         8,    1,    1 ) );
+
+    // This is the symbol used in this project to indicate character not found.
+    glyphs.insert( GlyphChecker(  0x7F,     7,      8,  169,  10,         8 ) );
+
+    // These are ISO-8859-1 codes that go beyond 127. Those characters might not display correctly on the terminal outputs.
+    glyphs.insert( GlyphChecker(  0xC4,     7,     11,  232,  10,         8,   0,   -3 ) );
+    glyphs.insert( GlyphChecker(  0xE4,     7,      7,  176,   2,         8,   0,    1 ) );
+    glyphs.insert( GlyphChecker(  0xD6,     7,     11,  240,  10,         8,   0,   -3 ) );
+    glyphs.insert( GlyphChecker(  0xF6,     7,      7,  184,   2,         8,   0,    1 ) );
+    glyphs.insert( GlyphChecker(  0xDC,     7,     11,  248,  10,         8,   0,   -3 ) );
+    glyphs.insert( GlyphChecker(  0xFC,     7,      7,  192,   2,         8,   0,    1 ) );
+
+    return glyphs;
+}
+
 int main() {
     int problem = 0;
+
+    const int OUTPUT_LEVEL = 0;
     
     {
-        auto windows_font_r = Data::Mission::FontResource::getWindows( &std::cout, 3 );
+        auto windows_font_r = Data::Mission::FontResource::getWindows( &std::cout, OUTPUT_LEVEL );
         std::string name = "Windows";
         
         if( windows_font_r == nullptr ) {
@@ -883,17 +1018,21 @@ int main() {
             return 1;
         }
         if( windows_font_r->getImage() == nullptr ) {
-            std::cout << name << " font lacks image!" << std::endl;
+            std::cout << name << " font lacks an image!" << std::endl;
             return 1;
         }
         
         problem |= checkImage( *windows_font_r, computer_qoi_len, computer_qoi, name );
+
+        auto expected_glyphs = generateComputerFont();
+
+        problem |= checkFont(*windows_font_r, expected_glyphs, name);
         
         delete windows_font_r;
     }
     
     {
-        auto mac_font_r = getMacintosh( &std::cout, 3 );
+        auto mac_font_r = getMacintosh( &std::cout, OUTPUT_LEVEL );
         std::string name = "Macintosh";
         
         if( mac_font_r == nullptr ) {
@@ -901,17 +1040,21 @@ int main() {
             return 1;
         }
         if( mac_font_r->getImage() == nullptr ) {
-            std::cout << name << " font lacks image!" << std::endl;
+            std::cout << name << " font lacks an image!" << std::endl;
             return 1;
         }
         
         problem |= checkImage( *mac_font_r, computer_qoi_len, computer_qoi, name );
+
+        auto expected_glyphs = generateComputerFont();
+
+        problem |= checkFont(*mac_font_r, expected_glyphs, name);
         
         delete mac_font_r;
     }
     
     {
-        auto ps1_font_r = Data::Mission::FontResource::getPlaystation( &std::cout, 3 );
+        auto ps1_font_r = Data::Mission::FontResource::getPlaystation( &std::cout, OUTPUT_LEVEL );
         std::string name = "Playstation";
         
         if( ps1_font_r == nullptr ) {
@@ -919,7 +1062,7 @@ int main() {
             return 1;
         }
         if( ps1_font_r->getImage() == nullptr ) {
-            std::cout << name << " font lacks image!" << std::endl;
+            std::cout << name << " font lacks an image!" << std::endl;
             return 1;
         }
         
