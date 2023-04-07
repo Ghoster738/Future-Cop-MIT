15dbe9da52820254ac8d8b474cc72b65cb2f17dd
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-04-06 20:49:10 -0700

    Simplification.

--- src/Tests/Utilities/Collision/GJKPolyhedron.cpp
+++ src/Tests/Utilities/Collision/GJKPolyhedron.cpp
@@ -47,28 +47,27 @@ int testSupport( std::string name, const GJKShape &shape, const std::vector<glm:
     return 0;
 }
 
-int testPolygon( std::string first_name, glm::vec3 position, std::vector<glm::vec3> cube_positon_data, std::vector<glm::vec3> cube_data ) {
-    //
+int testPolygon( std::string first_name, glm::vec3 position, std::vector<glm::vec3> poly_data ) {
     int status = SUCCESS;
     
     std::string name = "[" + first_name + " x = " + std::to_string( position.x ) + ", y = " + std::to_string( position.y ) + ", z = " + std::to_string( position.z ) + "]";
-    GJKPolyhedron cube( cube_positon_data );
+    GJKPolyhedron cube( poly_data );
     
     // Check support function for success.
-    for( size_t i = 0; i < cube_data.size(); i++ )
-        status |= testSupport( name, cube, cube_positon_data, i, glm::normalize(cube_data.at( i )), true );
+    for( size_t i = 0; i < poly_data.size() && status == SUCCESS; i++ )
+        status |= testSupport( name, cube, poly_data, i, glm::normalize(poly_data.at( i ) - position), true );
     
     // Check support function for failure.
-    for( size_t m = 0; m < cube_data.size(); m++ ) {
-        for( size_t n = 0; n < cube_data.size(); n++ ) {
+    for( size_t m = 0; m < poly_data.size() && status == SUCCESS; m++ ) {
+        for( size_t n = 0; n < poly_data.size() && status == SUCCESS; n++ ) {
             if( m != n ) {
-                status |= testSupport( name, cube, cube_positon_data, m, glm::normalize(cube_data.at( n )), false );
+                status |= testSupport( name, cube, poly_data, m, glm::normalize(poly_data.at( n ) - position), false );
             }
         }
     }
 
     // Check if the center is where it is supposed to be.
-    if( !glm::all( glm::equal(cube.getCenter(), position) ) ) {
+    if( !glm::all( glm::equal(cube.getCenter(), position) ) && status == SUCCESS ) {
         std::cout << "Center is wrong for \"" << name << "\"\n";
         displayVec3( "  Expected", position, std::cout );
         displayVec3( "  Result",   cube.getCenter(), std::cout );
@@ -85,14 +84,14 @@ int main() {
         std::string name = "cube";
         
         const glm::vec3 center = glm::vec3(0,0,0);
-        status |= testPolygon( name, center, generateCubeData( center ), generateCubeData() );
+        status |= testPolygon( name, center, generateCubeData( center ) );
         
         for( int x = -1; x <= 1 && status == SUCCESS; x++ ) {
             for( int y = -1; y <= 1 && status == SUCCESS; y++ ) {
                 for( int z = -1; z <= 1 && status == SUCCESS; z++ ) {
                     glm::vec3 scale(x, y, z);
-                    scale *= 4.0;
-                    status |= testPolygon( name, scale, generateCubeData( scale ), generateCubeData() );
+                    scale *= 3.0;
+                    status |= testPolygon( name, scale, generateCubeData( scale ) );
                 }
             }
         }
