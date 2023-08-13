4817128e71f55d6103749a91a9a7e7073dc7527f
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-08-13 15:09:15 -0700

    --window is maybe flawed

--- src/Tests/Utilities/Options/Parameters.cpp
+++ src/Tests/Utilities/Options/Parameters.cpp
@@ -48,20 +48,41 @@ public:
     }
 };
 
+void displayParameters( const Utilities::Options::Parameters &parameters, std::ostream &output ) {
+    output << "help       =  " << parameters.help.getValue()       << ", modified = " << parameters.help.wasModified() << "\n";
+    output << "fullScreen =  " << parameters.fullScreen.getValue() << ", modified = " << parameters.fullScreen.wasModified() << "\n";
+    output << "resWidth   =  " << parameters.resWidth.getValue()   << ", modified = " << parameters.resWidth.wasModified() << "\n";
+    output << "resHeight  =  " << parameters.resHeight.getValue()  << ", modified = " << parameters.resHeight.wasModified() << "\n";
+    output << "configPath =  " << parameters.configPath.getValue() << ", modified = " << parameters.configPath.wasModified() << "\n";
+    output << "userDir    =  " << parameters.userDir.getValue()    << ", modified = " << parameters.userDir.wasModified() << "\n";
+    output << "dataDir    =  " << parameters.dataDir.getValue()    << ", modified = " << parameters.dataDir.wasModified() << std::endl;
+}
+
+template<class I>
+int testModParam( int status, const I &param, const std::string &name, const std::string &test_name, std::ostream &output ) {
+    if( status == 0 && param.wasModified() ) {
+        output << "Error: " << name << " was modified in \"" << test_name << "\" case when it should not of.\n";
+
+        return 1;
+    }
+    else
+        return 0;
+}
+
 }
 
 int main( int argc, char *argv[] ) {
     int problem = 0;
 
-    std::string name = "No Name";
+    std::string program_name = "No Name";
 
     if( argc > 0 )
-        name = argv[0];
+        program_name = argv[0];
 
     {
         std::string param[] = {"LOL", "lolip"};
 
-        ParametersGiven parameters( {name, param[0], param[1]} );
+        ParametersGiven parameters( {program_name, param[0], param[1]} );
 
         if( parameters.getParamPointers() == nullptr ) {
             std::cout << "parameters.getParamPointers() should not be null." << std::endl;
@@ -73,8 +94,8 @@ int main( int argc, char *argv[] ) {
         }
 
         if( parameters.getParamPointers() != nullptr ) {
-            if( std::string( parameters.getParamPointers()[0] ) != name ) {
-                std::cout << "parameters.getParamPointers()[0] should be \"" << name << "\" not \"" << parameters.getParamPointers()[0] << "\"." << std::endl;
+            if( std::string( parameters.getParamPointers()[0] ) != program_name ) {
+                std::cout << "parameters.getParamPointers()[0] should be \"" << program_name << "\" not \"" << parameters.getParamPointers()[0] << "\"." << std::endl;
                 problem = 1;
             }
             if( std::string( parameters.getParamPointers()[1] ) != param[0] ) {
@@ -87,9 +108,183 @@ int main( int argc, char *argv[] ) {
             }
         }
     }
-    ParametersGiven no_name_parameters();
 
-    Utilities::Options::Parameters default_parameters( no_name_parameters.getParamAmount(), no_name_parameters.getParamPointers() );
+    {
+        std::string test_name = "no parameters";
+        ParametersGiven no_name_parameters;
+
+        Utilities::Options::Parameters default_parameters;
+        default_parameters.getParameters( no_name_parameters.getParamAmount(), no_name_parameters.getParamPointers(), std::cout );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.fullScreen, "FullScreen", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.resWidth,   "Width",      test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.resHeight,  "Height",     test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
+
+    {
+        std::string test_name = "valid fullscreen";
+        ParametersGiven parameters;
+
+        parameters.addArgument( program_name );
+        parameters.addArgument( "--fullscreen" );
+
+        Utilities::Options::Parameters default_parameters;
+        default_parameters.getParameters( parameters.getParamAmount(), parameters.getParamPointers(), std::cout );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        if( found_problem == 0 && !default_parameters.fullScreen.wasModified() && default_parameters.fullScreen.getValue() == true) {
+            std::cout << "Error: FullScreen was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        found_problem |= testModParam( found_problem, default_parameters.resWidth,   "Width",      test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.resHeight,  "Height",     test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
+
+    {
+        std::string test_name = "valid windowed";
+        ParametersGiven parameters;
+
+        parameters.addArgument( program_name );
+        parameters.addArgument( "--window" );
+
+        Utilities::Options::Parameters default_parameters;
+        default_parameters.getParameters( parameters.getParamAmount(), parameters.getParamPointers(), std::cout );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        if( found_problem == 0 && !default_parameters.fullScreen.wasModified() && default_parameters.fullScreen.getValue() == false) {
+            std::cout << "Error: FullScreen was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        found_problem |= testModParam( found_problem, default_parameters.resWidth,   "Width",      test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.resHeight,  "Height",     test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
+
+    {
+        std::string test_name = "--width valid test";
+        ParametersGiven parameters;
+
+        parameters.addArgument( program_name );
+        parameters.addArgument( "--width" );
+        parameters.addArgument( "720" );
+
+        Utilities::Options::Parameters default_parameters( parameters.getParamAmount(), parameters.getParamPointers() );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.fullScreen, "FullScreen", test_name, std::cout );
+        if( found_problem == 0 && !default_parameters.resWidth.wasModified() && default_parameters.resWidth.getValue() == 720) {
+            std::cout << "Error: Width was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        found_problem |= testModParam( found_problem, default_parameters.resHeight,  "Height",     test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
+
+    {
+        std::string test_name = "--height valid test";
+        ParametersGiven parameters;
+
+        parameters.addArgument( program_name );
+        parameters.addArgument( "--height" );
+        parameters.addArgument( "480" );
+
+        Utilities::Options::Parameters default_parameters( parameters.getParamAmount(), parameters.getParamPointers() );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.fullScreen, "FullScreen", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.resWidth,   "Width",      test_name, std::cout );
+        if( found_problem == 0 && !default_parameters.resHeight.wasModified() && default_parameters.resHeight.getValue() == 480) {
+            std::cout << "Error: Width was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
+
+    {
+        std::string test_name = "--res valid test";
+        ParametersGiven parameters;
+
+        parameters.addArgument( program_name );
+        parameters.addArgument( "--res" );
+        parameters.addArgument( "720x480" );
+
+        Utilities::Options::Parameters default_parameters( parameters.getParamAmount(), parameters.getParamPointers() );
+
+        int found_problem = 0;
+
+        found_problem |= testModParam( found_problem, default_parameters.help,       "Help",       test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.fullScreen, "FullScreen", test_name, std::cout );
+        if( found_problem == 0 && !default_parameters.resWidth.wasModified() && default_parameters.resWidth.getValue() == 720) {
+            std::cout << "Error: Width was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        if( found_problem == 0 && !default_parameters.resHeight.wasModified() && default_parameters.resHeight.getValue() == 480) {
+            std::cout << "Error: Height was not modified or set properly in \"" << test_name << "\" case when it should of.\n";
+
+            found_problem |= 1;
+        }
+        found_problem |= testModParam( found_problem, default_parameters.configPath, "ConfigPath", test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.userDir,    "UserDir",    test_name, std::cout );
+        found_problem |= testModParam( found_problem, default_parameters.dataDir,    "DataDir",    test_name, std::cout );
+
+        if( found_problem ) {
+            displayParameters( default_parameters, std::cout );
+            problem |= 1;
+        }
+    }
     
     return problem;
 }
