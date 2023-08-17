e47a2b148f7e9fc6071f9942182746efc75f6635
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-08-16 23:08:00 -0700

    Designing a main class

--- src/FCopMIT.cpp
+++ src/FCopMIT.cpp
@@ -28,13 +28,66 @@
 #include "ConfigureInput.h"
 #include "SplashScreens.h"
 
+class MainProgram {
+protected:
+    // Read the parameter system.
+    Utilities::Options::Parameters parameters;
+    Utilities::Options::Paths      paths;
+    Utilities::Options::Options    options;
+
+    // Data gathered from the options.
+    std::string program_name;
+
+    Data::Manager manager;
+    Data::Manager::Platform platform;
+    Data::Mission::IFF *global_r;
+    Data::Mission::IFF *resource_r;
+
+    //
+    Graphics::Environment *environment_p;
+    Controls::System *control_system_p;
+
+public:
+    MainProgram( int argc, char** argv ) : parameters( argc, argv ), paths( parameters ), options( paths, parameters ) {
+
+        // Set everything to null.
+        this->global_r         = nullptr;
+        this->resource_r       = nullptr;
+        this->environment_p    = nullptr;
+        this->control_system_p = nullptr;
+
+        // Get the program name first.
+        if( argc == 0 )
+            this->program_name = "ErrorNoParameters";
+        else
+            this->program_name = argv[ 0 ];
+
+        // Setup the professional logger next.
+        Utilities::logger.setOutputLog( &std::cout, 0, Utilities::Logger::INFO );
+
+        {
+            auto initialize_log = Utilities::logger.getLog( Utilities::Logger::ALL );
+            initialize_log.output << program_name << " started at ";
+        }
+
+        Utilities::logger.setTimeStampMode( true );
+    }
+
+    virtual ~MainProgram() {
+        if( control_system_p != nullptr )
+            delete control_system_p;
+
+        if( environment_p != nullptr )
+            delete environment_p;
+    }
+};
 
 int main(int argc, char** argv)
 {
     try {
-        Utilities::Options::Parameters parameters(argc, argv);
-        Utilities::Options::Paths paths(parameters);
-        Utilities::Options::Options options(paths, parameters);
+        Utilities::Options::Parameters parameters( argc, argv );
+        Utilities::Options::Paths paths( parameters );
+        Utilities::Options::Options options( paths, parameters );
 
         // Stop if help was requested, the information was already printed out
         if (parameters.help.getValue()) {
