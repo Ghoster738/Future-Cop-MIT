ac0b71d7cb5dda2b02dfadb03ae4520f218a8bd3
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-08-28 16:02:59 -0700

    MapSelectorMenu can now be exited.
    This is to prevent the user being traped when there are no resources
    being read.

--- src/MapSelectorMenu.cpp
+++ src/MapSelectorMenu.cpp
@@ -3,10 +3,17 @@
 #include <glm/vec4.hpp>
 
 #include "MainProgram.h"
+#include "MainMenu.h"
 
 namespace {
 void nullPress( MainProgram &main_program, Menu*, Menu::Item* ) {
     // Nothing.
+}
+void exitMapSelector( MainProgram &main_program, Menu* menu_r, Menu::Item* item_r ) {
+    if( main_program.menu_r != nullptr )
+        main_program.menu_r->unload( main_program );
+
+    main_program.menu_r = &MainMenu::main_menu;
 }
 void mapSelect( MainProgram &main_program, Menu* menu_r, Menu::Item* item_r ) {
     if( !Utilities::Options::Tools::isFile( main_program.manager.getIFFEntry( item_r->name ).getPath( main_program.platform ) ) )
@@ -33,13 +40,15 @@ void MapSelectorMenu::load( MainProgram &main_program ) {
 
     failed_map_name = "";
 
-    this->items.resize( Data::Manager::AMOUNT_OF_IFF_IDS + 1 );
+    this->items.resize( Data::Manager::AMOUNT_OF_IFF_IDS + 2 );
 
-    this->items[ Data::Manager::AMOUNT_OF_IFF_IDS ] = Menu::Item( this->name, glm::vec2( 0, 0 ), nullptr, nullptr, nullptr, nullptr, nullPress );
+    this->items[ Data::Manager::AMOUNT_OF_IFF_IDS + 0 ] = Menu::Item( "Back",     glm::vec2( 0, (Data::Manager::AMOUNT_OF_IFF_IDS + 1) * 24 ), &items[ Data::Manager::AMOUNT_OF_IFF_IDS - 1 ], nullptr, &items[ 0 ], nullptr, exitMapSelector );
+    this->items[ Data::Manager::AMOUNT_OF_IFF_IDS + 1 ] = Menu::Item( this->name, glm::vec2( 0, 0 ), nullptr, nullptr, nullptr, nullptr, nullPress );
 
-    this->items[0] = Menu::Item( *Data::Manager::map_iffs[0], glm::vec2( 0, 24 ), &items[ Data::Manager::AMOUNT_OF_IFF_IDS - 1 ], nullptr, &items[ 1 ], nullptr, mapSelect );
-    for( size_t i = 1; i < Data::Manager::AMOUNT_OF_IFF_IDS; i++ )
+    this->items[0] = Menu::Item( *Data::Manager::map_iffs[0], glm::vec2( 0, 24 ), &items[ Data::Manager::AMOUNT_OF_IFF_IDS ], nullptr, &items[ 1 ], nullptr, mapSelect );
+    for( size_t i = 1; i < Data::Manager::AMOUNT_OF_IFF_IDS - 1; i++ )
         this->items[i] = Menu::Item( *Data::Manager::map_iffs[i], glm::vec2( 0, (i + 1) * 24 ), &items[ (i - 1) % Data::Manager::AMOUNT_OF_IFF_IDS ], nullptr, &items[ (i + 1) % Data::Manager::AMOUNT_OF_IFF_IDS ], nullptr, mapSelect );
+    this->items[ Data::Manager::AMOUNT_OF_IFF_IDS - 1 ] = Menu::Item( *Data::Manager::map_iffs[ Data::Manager::AMOUNT_OF_IFF_IDS - 1 ], glm::vec2( 0, Data::Manager::AMOUNT_OF_IFF_IDS * 24 ), &items[ Data::Manager::AMOUNT_OF_IFF_IDS - 2 ], nullptr, &items[ Data::Manager::AMOUNT_OF_IFF_IDS ], nullptr, mapSelect );
 
     this->current_item_r = &items[0];
 }
@@ -54,29 +63,29 @@ void MapSelectorMenu::display( MainProgram &main_program ) {
     if( !failed_map_name.empty() ) {
         text_2d_buffer_r->setFont( 2 );
         text_2d_buffer_r->setColor( glm::vec4( 0.7, 0, 0.5, 1 ) );
-        text_2d_buffer_r->setPosition( glm::vec2( 100, 3 ) );
+        text_2d_buffer_r->setPosition( glm::vec2( 150, 3 ) );
         text_2d_buffer_r->print( "Game files are not found!" );
         text_2d_buffer_r->setColor( glm::vec4( 1, 1, 1, 1 ) );
-        text_2d_buffer_r->setPosition( glm::vec2( 100, 13 ) );
+        text_2d_buffer_r->setPosition( glm::vec2( 150, 13 ) );
         text_2d_buffer_r->print( "This project requires these game files to run." );
-        text_2d_buffer_r->setPosition( glm::vec2( 100, 35 ) );
+        text_2d_buffer_r->setPosition( glm::vec2( 150, 35 ) );
         text_2d_buffer_r->print( "This is the path that this program expects" );
-        text_2d_buffer_r->setPosition( glm::vec2( 100, 47 ) );
+        text_2d_buffer_r->setPosition( glm::vec2( 150, 47 ) );
         text_2d_buffer_r->print( "relative to the file location of this program." );
 
         auto entry = main_program.manager.getIFFEntry( failed_map_name );
         text_2d_buffer_r->setColor( glm::vec4( 0.5, 1, 0.5, 1 ) );
-        text_2d_buffer_r->setPosition( glm::vec2( 100, 59 ) );
+        text_2d_buffer_r->setPosition( glm::vec2( 150, 59 ) );
         text_2d_buffer_r->print( entry.getPath( main_program.platform ) );
 
         entry = main_program.manager.getIFFEntry( Data::Manager::global );
 
         if( entry.getIFF( main_program.platform ) == nullptr ) {
             text_2d_buffer_r->setColor( glm::vec4( 1, 1, 1, 1 ) );
-            text_2d_buffer_r->setPosition( glm::vec2( 100, 83 ) );
+            text_2d_buffer_r->setPosition( glm::vec2( 150, 83 ) );
             text_2d_buffer_r->print( "The globals file is also missing." );
             text_2d_buffer_r->setColor( glm::vec4( 0.5, 1, 0.5, 1 ) );
-            text_2d_buffer_r->setPosition( glm::vec2( 100, 95 ) );
+            text_2d_buffer_r->setPosition( glm::vec2( 150, 95 ) );
             text_2d_buffer_r->print( entry.getPath( main_program.platform ) );
         }
     }
