108a5f51c9849caedc3370e7ed22d955baac7cda
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-09-02 23:01:51 -0700

    Improved the title screen and made it scalable.

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -55,22 +55,31 @@ void MainMenu::load( MainProgram &main_program ) {
     glm::u32vec2 scale = main_program.getWindowScale();
     uint32_t center = scale.x / 2;
 
+    Graphics::Text2DBuffer::Font title_font;
     Graphics::Text2DBuffer::Font prime_font;
     Graphics::Text2DBuffer::Font selected_font;
 
-    if( !main_program.text_2d_buffer_r->selectFont( prime_font, 19, 27 ) )
+    const unsigned step = 2. / 30. * static_cast<float>( scale.y );
+
+    if( !main_program.text_2d_buffer_r->selectFont( title_font, 1.5 * step, 2. * step ) )
+        title_font = {1, 4.0};
+
+    if( !main_program.text_2d_buffer_r->selectFont( prime_font, 0.8 * step, 0.9 * step - 1 ) )
         prime_font = {1, 2.0};
 
-    if( !main_program.text_2d_buffer_r->selectFont( selected_font, 28, 31 ) )
+    if( !main_program.text_2d_buffer_r->selectFont( selected_font, 0.9 * step, step ) )
         selected_font = {1, 2.25};
 
     this->items.clear();
 
     if( !this->is_game_on ) {
-        this->items.emplace_back( new Menu::TextButton( "Map Spectator",    glm::vec2( center,  0 ), 3, 0, 1, 0, mapSpectator,   prime_font, selected_font ) );
-        this->items.emplace_back( new Menu::TextButton( "View Game Models", glm::vec2( center, 32 ), 0, 1, 2, 1, viewGameModels, prime_font, selected_font ) );
-        this->items.emplace_back( new Menu::TextButton( "Options",          glm::vec2( center, 64 ), 1, 2, 3, 2, options,        prime_font, selected_font ) );
-        this->items.emplace_back( new Menu::TextButton( "Exit to OS",       glm::vec2( center, 96 ), 2, 3, 0, 3, exitGame,       prime_font, selected_font ) );
+        this->items.emplace_back( new Menu::TextButton( "Map Spectator",    glm::vec2( center, scale.y - 6 * step ), 3, 0, 1, 0, mapSpectator,   prime_font, selected_font ) );
+        this->items.emplace_back( new Menu::TextButton( "View Game Models", glm::vec2( center, scale.y - 5 * step ), 0, 1, 2, 1, viewGameModels, prime_font, selected_font ) );
+        this->items.emplace_back( new Menu::TextButton( "Options",          glm::vec2( center, scale.y - 4 * step ), 1, 2, 3, 2, options,        prime_font, selected_font ) );
+        this->items.emplace_back( new Menu::TextButton( "Exit to OS",       glm::vec2( center, scale.y - 3 * step ), 2, 3, 0, 3, exitGame,       prime_font, selected_font ) );
+
+        this->items.emplace_back( new Menu::TextButton( "Future Cop:",  glm::vec2( center, 3 * step ), 0, 0, 0, 0, mapSpectator, title_font, title_font ) );
+        this->items.emplace_back( new Menu::TextButton( "MIT",          glm::vec2( center, 5 * step ), 0, 0, 0, 0, mapSpectator, title_font, title_font ) );
         this->current_item_index = 0;
     }
     else {
