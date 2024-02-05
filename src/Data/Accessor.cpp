0adfe9b7d7ab39f6cf4fb448769d1a99a410dd60
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-02-05 15:32:05 -0800

    Accessor is fully implemented, but also untested...
    C-Style macros can be very powerful...

--- src/Data/Accessor.cpp
+++ src/Data/Accessor.cpp
@@ -15,6 +15,17 @@
 #include "Mission/TilResource.h"
 #include "Mission/WAVResource.h"
 
+#define SEARCH( CLASS_NAME ) \
+    Mission::Resource *resource_r = nullptr;\
+    SearchValue search_value = {  Mission::CLASS_NAME::IDENTIFIER_TAG, resource_id };\
+\
+    auto result = search.find(search_value);\
+\
+    if( result != search.end() )\
+        resource_r = (*result).second;\
+\
+    return dynamic_cast<Mission::CLASS_NAME*>( resource_r );
+
 namespace Data {
 
 bool Accessor::SearchValue::operator< ( const Accessor::SearchValue & operand ) const {
@@ -43,68 +54,59 @@ Accessor::Accessor( std::vector<Mission::IFF*> resources ) {
 Accessor::~Accessor() {}
 
 Mission::ANMResource* Accessor::getANM( uint32_t resource_id ) const {
-
+    SEARCH(ANMResource)
 }
 
 Mission::BMPResource* Accessor::getBMP( uint32_t resource_id ) const {
-
+    SEARCH(BMPResource)
 }
 
 Mission::DCSResource* Accessor::getDCS( uint32_t resource_id ) const {
-
+    SEARCH(DCSResource)
 }
 
 Mission::FUNResource* Accessor::getFUN( uint32_t resource_id ) const {
-
+    SEARCH(FUNResource)
 }
 
 Mission::FontResource* Accessor::getFNT( uint32_t resource_id ) const {
-
+    SEARCH(FontResource)
 }
 
 Mission::MSICResource* Accessor::getMISC( uint32_t resource_id ) const {
-
+    SEARCH(MSICResource)
 }
 
 Mission::NetResource* Accessor::getNET( uint32_t resource_id ) const {
-
+    SEARCH(NetResource)
 }
 
 Mission::ObjResource* Accessor::getOBJ( uint32_t resource_id ) const {
-    Mission::Resource *resource_r = nullptr;
-    SearchValue search_value = { Mission::ObjResource::IDENTIFIER_TAG, resource_id };
-
-    auto result = search.find(search_value);
-
-    if( result != search.end() ) {
-        resource_r = (*result).second;
-    }
-
-    return dynamic_cast<Mission::ObjResource*>( resource_r );
+    SEARCH(ObjResource)
 }
 
 Mission::PTCResource* Accessor::getPTC( uint32_t resource_id ) const {
-
+    SEARCH(PTCResource)
 }
 
 Mission::PYRResource* Accessor::getPYR( uint32_t resource_id ) const {
-
+    SEARCH(PYRResource)
 }
 
 Mission::RPNSResource* Accessor::getRPNS( uint32_t resource_id ) const {
-
+    SEARCH(RPNSResource)
 }
 
 Mission::SNDSResource* Accessor::getSNDS( uint32_t resource_id ) const {
-
+    SEARCH(SNDSResource)
 }
 
 Mission::TilResource* Accessor::getTIL( uint32_t resource_id ) const {
-
+    SEARCH(TilResource)
 }
 
 Mission::WAVResource* Accessor::getWAV( uint32_t resource_id ) const {
-
+    SEARCH(WAVResource)
 }
 
 
