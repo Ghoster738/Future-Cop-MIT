db667ced4ef13b0e825d09f7bca57ae77a86f728
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-04-19 18:12:28 -0700

    I am not sure if this is useful, but it is there.

--- src/Graphics/SDL2/GLES2/Internal/MorphModelDraw.cpp
+++ src/Graphics/SDL2/GLES2/Internal/MorphModelDraw.cpp
@@ -243,6 +243,8 @@ void Graphics::SDL2::GLES2::Internal::MorphModelDraw::draw( Graphics::SDL2::GLES
                 const auto texture_offset = (*instance)->getTextureOffset();
                 glUniform2f( this->texture_offset_uniform_id, texture_offset.x, texture_offset.y );
 
+                (*d).second->bindUVAnimation(animated_uv_frames_id, (*instance)->getTextureTransformTimeline());
+
                 // Get the position and rotation of the model.
                 // Multiply them into one matrix which will hold the entire model transformation.
                 camera_3D_model_transform = glm::translate( glm::mat4(1.0f), (*instance)->getPosition() ) * glm::toMat4( (*instance)->getRotation() );
