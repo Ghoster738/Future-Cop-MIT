f59bb9b40e277ad7a651c93da3c05f895380d260
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-05-26 11:56:45 -0700

    TOSResource has an error system and warning system.
    To tell the programmer, me, if there is anything that is wrong with my
    TOS reading algorithm or knowledge.

--- src/Data/Mission/TOSResource.cpp
+++ src/Data/Mission/TOSResource.cpp
@@ -49,13 +49,48 @@ bool TOSResource::parse( const ParseSettings &settings ) {
 
         const auto number_of_entries = reader.readU32( settings.endian );
 
+        const uint64_t size_of_data = (4 + 3 * number_of_entries) * sizeof(uint32_t);
+
+        if(size_of_data > reader.totalSize() ) {
+            auto error_log = settings.logger_r->getLog( Utilities::Logger::ERROR );
+            error_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
+
+            error_log.output << "The TOS resource did not parse!. Chunk too small.\n";
+            error_log.output << "  size_of_chunk = " << size_of_chunk << "\n";
+            error_log.output << "  number_of_entries = " << number_of_entries << "\n";
+            error_log.output << "  estimated_size_of_data = " << size_of_data << "\n";
+            error_log.output << "  actual_size_of_data = " << reader.totalSize() << "\n";
+
+            return false;
+        }
+        else if(size_of_data < reader.totalSize()) {
+            auto warning_log = settings.logger_r->getLog( Utilities::Logger::WARNING );
+            warning_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
+
+            warning_log.output << "The TOS resource has data that is not read.\n";
+            warning_log.output << "  size_of_chunk = " << size_of_chunk << "\n";
+            warning_log.output << "  number_of_entries = " << number_of_entries << "\n";
+            warning_log.output << "  estimated_size_of_data = " << size_of_data << "\n";
+            warning_log.output << "  actual_size_of_data = " << reader.totalSize() << "\n";
+        }
+
+        bool found_problem = false;
+
         for(uint32_t n = 0; n < number_of_entries; n++) {
             const auto unknown  = reader.readU32( settings.endian );
             const auto unused_0 = reader.readU32( settings.endian );
             const auto unused_1 = reader.readU32( settings.endian );
 
-            //assert(unused_0 == 0);
-            //assert(unused_1 == 0);
+            if(!found_problem && (unused_0 != 0 || unused_1 != 0)) {
+                auto warning_log = settings.logger_r->getLog( Utilities::Logger::WARNING );
+                warning_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
+
+                warning_log.output << "The TOS resource has data that is not read at entry " << n << ".\n" << std::hex;
+                warning_log.output << "  unused_0 = 0x" << unused_0 << ".\n";
+                warning_log.output << "  unused_1 = 0x" << unused_1 << ".\n";
+
+                found_problem = true;
+            }
         }
 
         return true;
