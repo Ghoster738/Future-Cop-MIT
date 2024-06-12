761dbd1cbea31c99ec5ae160906640388473b21a
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-06-11 21:11:33 -0700

    Revised WAVResource reading.
    Also, SMPL chunk is read, but not processed.

--- src/Data/Mission/WAVResource.cpp
+++ src/Data/Mission/WAVResource.cpp
@@ -6,90 +6,227 @@
 namespace {
     const uint32_t TAG_CHUNK_ID = 0x52494646; // which is { 0x52, 0x49, 0x46, 0x46 } or { 'R', 'I', 'F', 'F' } or "RIFF"
     const uint32_t TAG_FORMAT = 0x57415645; // which is { 0x57, 0x41, 0x56, 0x45 } or { 'W', 'A', 'V', 'E' } or "WAVE"
-    const uint32_t TAG_SUB_CHUNK_1_ID = 0x666d7420; // which is { 0x66, 0x6d, 0x74, 0x20 } or { 'f', 'm', 't', ' ' } or "fmt "
-    const uint32_t TAG_SUB_CHUNK_2_ID = 0x64617461; // which is { 0x64, 0x61, 0x74, 0x61 } or { 'd', 'a', 't', 'a' } or "data"
+    const uint32_t TAG_FMT_ID = 0x666d7420; // which is { 0x66, 0x6d, 0x74, 0x20 } or { 'f', 'm', 't', ' ' } or "fmt "
+    const uint32_t TAG_DATA_ID = 0x64617461; // which is { 0x64, 0x61, 0x74, 0x61 } or { 'd', 'a', 't', 'a' } or "data"
+    const uint32_t TAG_SMPL_ID = 0x736D706C; // which is { 0x73, 0x6D, 0x70, 0x6C } or { 's', 'm', 'p', 'l' } or "smpl"
     const size_t DATA_START_FROM_HEADER = 0x2C;
+    const size_t LOOP_LIMIT = 0x10;
+
+    struct FMTData {
+        uint16_t format_tag;
+        uint16_t num_channels;
+        uint32_t samples_per_sec;
+        uint32_t avg_bytes_per_sec;
+        uint16_t block_align;
+        uint16_t bits_per_sample; // This is present for PCM Wave files.
+    };
+    struct SampleLoopData {
+        uint32_t id;
+        uint32_t type;
+        uint32_t start;
+        uint32_t end;
+        uint32_t fraction;
+        uint32_t repeat_amount; // 0 means infinity.
+    };
+    struct SampleData {
+        uint32_t manufacturer;
+        uint32_t product;
+        uint32_t sample_period_nanoseconds;
+        uint32_t midi_note;
+        uint32_t smpte_format;
+        uint32_t smpte_offset;
+        std::vector<SampleLoopData> loop_data;
+    };
 }
 
 bool Data::Mission::WAVResource::parse( const ParseSettings &settings ) {
     auto debug_log = settings.logger_r->getLog( Utilities::Logger::DEBUG );
     debug_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
+    auto warning_log = settings.logger_r->getLog( Utilities::Logger::WARNING );
+    debug_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
     auto error_log = settings.logger_r->getLog( Utilities::Logger::ERROR );
     error_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
 
-    if( this->data_p != nullptr ) {
-        auto reader = this->data_p->getReader();
-        
-        // Check to see if there is enough data for the WAV file to be passable.
-        // This is to check for buffer overflow attacks and the like.
-        if( reader.totalSize() > DATA_START_FROM_HEADER ) {
-            bool file_is_not_valid = false;
-            
-            auto tag_chunk_id   = reader.readU32( Utilities::Buffer::Endian::BIG ); // 0
-            auto tag_chunk_size = reader.readU32( Utilities::Buffer::Endian::LITTLE ); // 4
-            auto tag_format     = reader.readU32( Utilities::Buffer::Endian::BIG ); // 8
-            auto tag_sub_1      = reader.readU32( Utilities::Buffer::Endian::BIG ); // 12
-            auto size_of_chunk_1 = reader.readU32( Utilities::Buffer::Endian::LITTLE ); // 16
-            auto un_size_sub_1      = reader.readU32( Utilities::Buffer::Endian::LITTLE );
-            reader.setPosition( 36, Utilities::Buffer::BEGIN );
-            auto tag_sub_2      = reader.readU32( Utilities::Buffer::Endian::BIG );
+    if( this->data_p == nullptr ) {
+        error_log.output << "WAV file data is missing!\n";
+        return false;
+    }
 
-            file_is_not_valid |= (tag_chunk_id    != TAG_CHUNK_ID );
-            file_is_not_valid |= (tag_format      != TAG_FORMAT );
-            file_is_not_valid |= (tag_sub_1       != TAG_SUB_CHUNK_1_ID );
-            file_is_not_valid |= (size_of_chunk_1 != 16); // This loader only supports size 16
-            file_is_not_valid |= (tag_sub_2       != TAG_SUB_CHUNK_2_ID );
-            file_is_not_valid |= (tag_chunk_size  >  reader.totalSize() - 8);
+    auto riff_reader = this->data_p->getReader();
 
-            if( !file_is_not_valid ) {
-                reader.setPosition( 20, Utilities::Buffer::BEGIN );
-                
-                audio_format        = reader.readU16( Utilities::Buffer::Endian::LITTLE );
-                channel_number      = reader.readU16( Utilities::Buffer::Endian::LITTLE );
-                sample_rate         = reader.readU32( Utilities::Buffer::Endian::LITTLE );
-                auto read_byte_rate = reader.readU32( Utilities::Buffer::Endian::LITTLE );
-                auto read_block_aln = reader.readU16( Utilities::Buffer::Endian::LITTLE );
-                bits_per_sample     = reader.readU16( Utilities::Buffer::Endian::LITTLE );
-                
-                reader.setPosition( 40, Utilities::Buffer::BEGIN );
-                audio_stream_length = reader.readU32( Utilities::Buffer::Endian::LITTLE );
+    auto riff_id    = riff_reader.readU32( Utilities::Buffer::Endian::BIG );    // 0
+    auto riff_size  = riff_reader.readU32( Utilities::Buffer::Endian::LITTLE ); // 4
 
-                // This sets block_align and byte_rate to their respective values
-                updateDependices();
+    if( riff_id != TAG_CHUNK_ID ) {
+        error_log.output << "This resource not RIFF formated thus it is not a WAVE file. Aborting!\n";
+        return false;
+    }
 
-                file_is_not_valid |= (  audio_stream_length > (reader.totalSize() - DATA_START_FROM_HEADER) );
-                file_is_not_valid |= (  byte_rate  != read_byte_rate );
-                file_is_not_valid |= ( block_align != read_block_aln );
+    if( riff_size < sizeof(uint32_t)) {
+        error_log.output << "RIFF is way too small to store a WAV file.\n";
+        return false;
+    }
 
-                if( !file_is_not_valid ) {
-                    // Copy the rest of the sound data to the audio stream
-                    reader.setPosition( DATA_START_FROM_HEADER, Utilities::Buffer::BEGIN );
-                    
-                    setAudioStream( reader );
+    auto wav_reader = riff_reader.getReader( riff_size );
+    auto wav_id     = wav_reader.readU32( Utilities::Buffer::Endian::BIG );
 
-                    debug_log.output << "This is a wav file." << std::endl;
-                    
-                    return true;
-                }
-                else {
-                    error_log.output << "Potential buffer overflow attempt detected. Please at least scan it for viruses if you got this one from the internet!\n";
+    if( wav_id != TAG_FORMAT ) {
+        error_log.output << "This resource is RIFF formated, but it is not a WAVE file. Aborting!\n";
+        return false;
+    }
+
+    FMTData fmt_data;
+    bool found_fmt_chunk = false;
+    uint32_t sound_data_size = 0;
+    uint32_t sound_data_offset = 0;
+    bool found_data_chunk = false;
+    SampleData sample_data;
+    bool found_sample_chunk = false;
+
+    while( wav_reader.getPosition() < wav_reader.totalSize() ) {
+        auto identifier = wav_reader.readU32( Utilities::Buffer::Endian::BIG );
+        auto tag_size   = wav_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+
+        auto after_tag_offset = wav_reader.getPosition();
+
+        auto chunk_reader = wav_reader.getReader( tag_size );
+
+        switch(identifier) {
+            case TAG_FMT_ID:
+            {
+                if(found_fmt_chunk) {
+                    error_log.output << "Only one FMT chunk for each WAV file.\n";
                     return false;
                 }
+
+                fmt_data.format_tag        = chunk_reader.readU16( Utilities::Buffer::Endian::LITTLE );
+                fmt_data.num_channels      = chunk_reader.readU16( Utilities::Buffer::Endian::LITTLE );
+                fmt_data.samples_per_sec   = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                fmt_data.avg_bytes_per_sec = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                fmt_data.block_align       = chunk_reader.readU16( Utilities::Buffer::Endian::LITTLE );
+
+                if(fmt_data.format_tag != 1) { // Microsoft PCM Wave files only.
+                    error_log.output << "This WAV file should only be PCM data.\n";
+                    return false;
+                }
+
+                if(fmt_data.num_channels > 2 || fmt_data.num_channels == 0) {
+                    error_log.output << "Only two channels supported, not " << fmt_data.num_channels << ".\n";
+                    return false;
+                }
+
+                fmt_data.bits_per_sample = chunk_reader.readU16( Utilities::Buffer::Endian::LITTLE );
+
+                if( fmt_data.bits_per_sample != 16 && fmt_data.bits_per_sample != 8 ) {
+                    error_log.output << "Sample bit size " << std::dec << fmt_data.bits_per_sample << " is not supported.\n";
+                    return false;
+                }
+
+                found_fmt_chunk = true;
             }
-            else
+            break;
+            case TAG_DATA_ID:
             {
-                if( size_of_chunk_1 == 16 )
-                    error_log.output << "This is not a wav file.\n";
-                else
-                    error_log.output << "This pariticalar wav file's format is not supported.\n" ;
-                return false;
+                if(!found_fmt_chunk) {
+                    warning_log.output << "This WAVE file might not be according to specifications. FMT should be declared after the DATA chunk.\n";
+                }
+                if(found_data_chunk) {
+                    error_log.output << "Only one DATA chunk for each WAV file.\n";
+                    return false;
+                }
+
+                sound_data_size = tag_size;
+                sound_data_offset = after_tag_offset;
+
+                found_data_chunk = true;
             }
+            break;
+            case TAG_SMPL_ID:
+            {
+                if(found_sample_chunk) {
+                    error_log.output << "Only one Sample (smpl) chunk for each WAV file.\n";
+                    return false;
+                }
+
+                sample_data.manufacturer              = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                sample_data.product                   = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                sample_data.sample_period_nanoseconds = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                sample_data.midi_note                 = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                sample_data.smpte_format              = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                sample_data.smpte_offset              = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+
+                auto loop_amount = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+
+                if(loop_amount > LOOP_LIMIT) {
+                    warning_log.output << "Loop limit of " << LOOP_LIMIT << " reached.\n";
+                    warning_log.output << "The requested loop amount " << loop_amount << ", but is reduced to the loop limit.\n";
+
+                    loop_amount = LOOP_LIMIT;
+                }
+
+                sample_data.loop_data.resize(loop_amount);
+
+                for(size_t i = 0; i < sample_data.loop_data.size(); i++) {
+                    sample_data.loop_data[i].id            = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                    sample_data.loop_data[i].type          = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                    sample_data.loop_data[i].start         = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                    sample_data.loop_data[i].end           = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                    sample_data.loop_data[i].fraction      = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                    sample_data.loop_data[i].repeat_amount = chunk_reader.readU32( Utilities::Buffer::Endian::LITTLE );
+                }
+
+                found_sample_chunk = true;
+            }
+            break;
+            default:
+                error_log.output << "Unrecognized chunk " << static_cast<char>(identifier >> 24) << static_cast<char>(identifier >> 16) << static_cast<char>(identifier >> 8)<< static_cast<char>(identifier) << ".\n";
         }
-        else
-            return false;
     }
-    else
+
+    // Check for two required chunks.
+    if(!found_fmt_chunk) {
+        error_log.output << "Only one FMT chunk for each WAV file. This \"WAV\" file lacks this!\n";
         return false;
+    }
+    if(!found_data_chunk) {
+        error_log.output << "Only one DATA chunk for each WAV file. This \"WAV\" file lacks this!\n";
+        return false;
+    }
+
+    this->audio_format    = fmt_data.format_tag;
+    this->channel_number  = fmt_data.num_channels;
+    this->sample_rate     = fmt_data.samples_per_sec;
+    this->bits_per_sample = fmt_data.bits_per_sample;
+    updateDependices();
+
+    if(this->byte_rate != fmt_data.avg_bytes_per_sec) {
+        error_log.output << "The predicted byte rate does not match with what the WAV file had.\n";
+        error_log.output << "   predicted byte rate " << this->byte_rate << "\n";
+        error_log.output << "   wav byte rate " << fmt_data.avg_bytes_per_sec << "\n";
+        return false;
+    }
+    if(this->block_align != fmt_data.block_align) {
+        error_log.output << "The predicted block align does not match with what the WAV file had.\n";
+        error_log.output << "   predicted block align " << this->block_align << "\n";
+        error_log.output << "   wav block align " << fmt_data.block_align << "\n";
+        return false;
+    }
+
+    wav_reader.setPosition( sound_data_offset, Utilities::Buffer::BEGIN );
+    auto pcm_reader = wav_reader.getReader( sound_data_size );
+
+    if(fmt_data.bits_per_sample == 16) {
+        size_t head = 0;
+        audio_stream.resize( pcm_reader.totalSize() );
+        while( !pcm_reader.ended() ) {
+            reinterpret_cast<int16_t*>(audio_stream.data())[head] = pcm_reader.readI16( Utilities::Buffer::Endian::LITTLE );
+            head++;
+        }
+    }
+    else // if(fmt_data.bits_per_sample == 8) {
+        setAudioStream(pcm_reader);
+
+    return true;
 }
 
 const std::string Data::Mission::WAVResource::FILE_EXTENSION = "wav";
@@ -180,7 +317,7 @@ int Data::Mission::WAVResource::writeAudio( const std::string& file_path, bool i
         header.addU32( audio_stream.size() + DATA_START_FROM_HEADER - 8, Utilities::Buffer::Endian::LITTLE );
 
         header.addU32( TAG_FORMAT, Utilities::Buffer::Endian::BIG );
-        header.addU32( TAG_SUB_CHUNK_1_ID, Utilities::Buffer::Endian::BIG );
+        header.addU32( TAG_FMT_ID, Utilities::Buffer::Endian::BIG );
         header.addU32( 16, Utilities::Buffer::Endian::LITTLE );
         header.addU16( audio_format, Utilities::Buffer::Endian::LITTLE );
         header.addU16( channel_number, Utilities::Buffer::Endian::LITTLE );
@@ -189,7 +326,7 @@ int Data::Mission::WAVResource::writeAudio( const std::string& file_path, bool i
         header.addU16( block_align, Utilities::Buffer::Endian::LITTLE );
         header.addU16( bits_per_sample, Utilities::Buffer::Endian::LITTLE );
 
-        header.addU32( TAG_SUB_CHUNK_2_ID, Utilities::Buffer::Endian::BIG );
+        header.addU32( TAG_DATA_ID, Utilities::Buffer::Endian::BIG );
         header.addU32( audio_stream_length, Utilities::Buffer::Endian::LITTLE );
 
         auto reader = header.getReader();
