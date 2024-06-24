5a850d68e73c5b3847248e5087070e191544b27b
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-06-24 01:26:08 -0700

    Fixed compilation error

--- src/Sound/OpenAL/Internal/SoundSource .cpp
+++ src/Sound/OpenAL/Internal/SoundSource .cpp
@@ -7,7 +7,7 @@ namespace Internal {
 ALenum SoundSource::allocate(const Data::Mission::WAVResource &sound) {
     this->no_repeat_offset = true;
 
-    ALenum return_state = buffer_indexes[0].allocate(sound);
+    ALenum return_state = this->buffer_indexes[0].allocate(sound);
 
     if(return_state != AL_NO_ERROR)
         return return_state;
@@ -15,23 +15,25 @@ ALenum SoundSource::allocate(const Data::Mission::WAVResource &sound) {
     if(sound.hasLoop()) {
         this->no_repeat_offset = false;
 
-        buffer_indexes[1].allocate(sound, 0, sound.getLoopBeginSample());
-        buffer_indexes[2].allocate(sound, sound.getLoopBeginSample(), sound.getLoopEndSample());
+        this->buffer_indexes[1].allocate(sound, 0, sound.getLoopBeginSample());
+        this->buffer_indexes[2].allocate(sound, sound.getLoopBeginSample(), sound.getLoopEndSample());
     }
 
     return AL_NO_ERROR;
 }
 
 ALenum SoundSource::deallocate() {
-    ALenum return_state = buffer_indexes[0].deallocate();
+    ALenum return_state = this->buffer_indexes[0].deallocate();
 
-    if(sound.hasLoop()) {
-        buffer_indexes[1].deallocate();
-        buffer_indexes[2].deallocate();
+    if(!this->no_repeat_offset ) {
+        this->buffer_indexes[1].deallocate();
+        this->buffer_indexes[2].deallocate();
     }
 
     if(return_state != AL_NO_ERROR)
         return return_state;
+
+    return AL_NO_ERROR;
 }
 
 }
