55745937ae9443bac9e86a4c7712a3c178dd170c
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-06-25 01:50:42 -0700

    Listener internals thought of.

--- src/Sound/OpenAL/Listener.h
+++ src/Sound/OpenAL/Listener.h
@@ -2,12 +2,23 @@
 #define SOUND_OPENAL_LISTENER_H
 
 #include "../Listener.h"
+#include "Speaker.h"
+
+#include <vector>
 
 namespace Sound {
 namespace OpenAL {
 
 class Listener : public Sound::Listener {
 public:
+    struct Source {
+        Speaker *speaker_r;
+        ALuint queue_source;
+    };
+
+    size_t source_max_length;
+    std::vector<Source> sources;
+
     Listener(WhichEar which_ear = WhichEar::BOTH);
 
     virtual ~Listener();
