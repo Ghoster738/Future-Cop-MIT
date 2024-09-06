8ae1c8555203198063d5acbd9e4d845e9285d1f0
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-09-05 17:53:17 -0700

    This format is weird.

--- src/Data/Mission/SHDResource.cpp
+++ src/Data/Mission/SHDResource.cpp
@@ -1,10 +1,30 @@
 #include "SHDResource.h"
 
+#include <sstream>
+
 namespace Data {
 namespace Mission {
 
 const std::string SHDResource::FILE_EXTENSION = "shd";
 const uint32_t SHDResource::IDENTIFIER_TAG = 0x43736864; // which is { 0x43, 0x73, 0x68, 0x64 } or { 'C', 's', 'h', 'd' } or "Cshd"
+
+std::string SHDResource::Entry::getString() const {
+    std::stringstream form;
+
+    form << std::hex
+        << "group_id = 0x" << group_id << ", "
+        << "sound_id = 0x" << sound_id << ", "
+        << "unk_0 = 0x" << (uint16_t)unk_0 << ", "
+        << "unk_1 = 0x" << (uint16_t)unk_1 << ", "
+        << "loop  = 0x" << (uint16_t)loop << ", "
+        << "unk_2 = 0x" << (uint16_t)unk_2 << ", "
+        << "script_id = 0x" << (uint16_t)script_id << ", "
+        << "unk_3 = 0x" << (uint16_t)unk_3 << ", "
+        << "unk_4 = 0x" << (uint16_t)unk_4 << ", "
+        << "unk_5 = 0x" << (uint16_t)unk_5;
+
+    return form.str();
+}
 
 SHDResource::SHDResource() {}
 SHDResource::SHDResource( const SHDResource &obj ) : Resource( obj ), entries( obj.entries ) {}
@@ -22,6 +42,9 @@ bool SHDResource::noResourceID() const {
 }
 
 bool SHDResource::parse( const ParseSettings &settings ) {
+    auto error_log = settings.logger_r->getLog( Utilities::Logger::ERROR );
+    error_log.info << FILE_EXTENSION << ": " << getResourceID() << "\n";
+
     if( this->data_p != nullptr ) {
         auto reader = this->data_p->getReader();
 
@@ -33,21 +56,25 @@ bool SHDResource::parse( const ParseSettings &settings ) {
 
         this->entries.resize(count);
 
+        error_log.output << "Count = " << count << "\n";
+
         for( uint16_t i = 0; i < count; i++ ) {
-            this->entries[i].group_id = reader.readU16( settings.endian );
-            this->entries[i].sound_id = reader.readU16( settings.endian );
+            this->entries[i].group_id = reader.readU16( settings.endian ); // 0
+            this->entries[i].sound_id = reader.readU16( settings.endian ); // 2
 
-            this->entries[i].unk_0 = reader.readU8();
-            this->entries[i].unk_1 = reader.readU8();
+            this->entries[i].unk_0 = reader.readU8(); // 4
+            this->entries[i].unk_1 = reader.readU8(); // 5
 
-            this->entries[i].loop  = reader.readU8();
-            this->entries[i].unk_2 = reader.readU8();
+            this->entries[i].loop  = reader.readU8(); // 6
+            this->entries[i].unk_2 = reader.readU8(); // 7
 
-            this->entries[i].script_id = reader.readU8();
-            this->entries[i].unk_3     = reader.readU8();
+            this->entries[i].script_id = reader.readU8(); // 8
+            this->entries[i].unk_3     = reader.readU8(); // 9
 
-            this->entries[i].unk_4 = reader.readU8();
-            this->entries[i].unk_5 = reader.readU8();
+            this->entries[i].unk_4 = reader.readU8(); // 10
+            this->entries[i].unk_5 = reader.readU8(); // 11
+
+            error_log.output << this->entries[i].getString() << "\n";
         }
 
         return true;
--- src/Data/Mission/SHDResource.h
+++ src/Data/Mission/SHDResource.h
@@ -3,6 +3,9 @@
 
 #include "Resource.h"
 
+#include <string>
+#include <vector>
+
 namespace Data {
 
 namespace Mission {
@@ -13,8 +16,8 @@ public:
     static const uint32_t IDENTIFIER_TAG;
 
     struct Entry {
-        uint16_t group_id;
-        uint16_t sound_id;
+        uint16_t group_id; // Used for random!
+        uint16_t sound_id; // Resource ID
 
         uint8_t unk_0;
         uint8_t unk_1;
@@ -22,11 +25,13 @@ public:
         uint8_t loop;
         uint8_t unk_2;
 
-        uint8_t script_id;
+        uint8_t script_id; // ID used in script or assembly code.
         uint8_t unk_3;
 
         uint8_t unk_4;
         uint8_t unk_5;
+
+        std::string getString() const;
     };
 
 private:
