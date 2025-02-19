d79d7246082347379ebe68a3a78230ee9e96f462
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-02-19 15:16:02 -0800

    isFormat implemented

--- src/Utilities/ImageFormat/WindowsBitmap.cpp
+++ src/Utilities/ImageFormat/WindowsBitmap.cpp
@@ -1,6 +1,10 @@
 #include "WindowsBitmap.h"
 
-const std::string Utilities::ImageFormat::WindowsBitmap::FILE_EXTENSION = "bmp";
+namespace Utilities {
+
+namespace ImageFormat {
+
+const std::string WindowsBitmap::FILE_EXTENSION = "bmp";
 
 // 16 bit color for bitmap urrrrrgggggbbbbb
 // Low Blue   = 0x0001
@@ -9,3 +13,37 @@ const std::string Utilities::ImageFormat::WindowsBitmap::FILE_EXTENSION = "bmp";
 // High Green = 0x03E0
 // Low Red    = 0x0400
 // High Red   = 0x7C00
+
+WindowsBitmap::WindowsBitmap() {}
+
+WindowsBitmap::~WindowsBitmap() {}
+
+ImageFormat* WindowsBitmap::duplicate() const {
+    return new WindowsBitmap();
+}
+
+bool WindowsBitmap::isFormat( const Buffer& buffer ) const {
+    auto reader = buffer.getReader();
+    const size_t HEADER_STRUCT_SIZE =  0xE;
+    // const size_t   HEADER_STRUCT_SIZE = 0x28; // Not needed
+
+    // Check the header
+    if(reader.totalSize() <= INFO_STRUCT_SIZE)
+        return false;
+
+    if( reader.readI8() != 'B' || reader.readI8() != 'M' )
+        return false;
+
+    const auto BMP_SIZE = reader.readU32( Utilities::Buffer::Endian::LITTLE );
+
+    if(BMP_SIZE != reader.totalSize())
+        return false;
+
+    // WARNING this does not fully check if the BMP is correct for the parser.
+
+    return true;
+}
+
+}
+
+}
