18f0cef749f4db698d1841279e2d8c239f35e642
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-02-21 02:13:08 -0800

    Image almost done.

--- src/Graphics/SDL2/GLES2/Image.cpp
+++ src/Graphics/SDL2/GLES2/Image.cpp
@@ -1,18 +1,39 @@
 #include "Image.h"
+#include "Internal/ImageDraw.h"
 
-namespace Graphics {
-namespace SDL2 {
-namespace GLES2 {
+namespace Graphics::SDL2::GLES2 {
 
 Graphics::Image* Environment::allocateImage() {
-    return nullptr;
+    auto image_p = new Graphics::SDL2::GLES2::Image();
+
+    // ImageBase
+    image_p->positions[0]      = glm::vec2(-1.0, -1.0);
+    image_p->positions[1]      = glm::vec2( 1.0,  1.0);
+    image_p->color             = glm::vec4( 1.0,  1.0,  1.0,  1.0);
+    image_p->is_visable        = false;
+
+    // Image
+    image_p->cbmp_id           = 1;
+    image_p->texture_coords[0] = glm::vec2( 0.0,  0.0);
+    image_p->texture_coords[1] = glm::vec2( 1.0,  1.0);
+
+    // OpenGL Info
+    image_p->environment_r = this;
+
+    return image_p;
 }
 
 Image::~Image() {}
 
-void Image::update() {}
+void Image::update() {
+    Internal::ImageDraw::ImageData image_data;
 
+    image_data.positions[0]      = this->positions[0];
+    image_data.positions[1]      = this->positions[1];
+    image_data.color             = this->color;
+    image_data.visable           = this->is_visable;
+    image_data.texture_coords[0] = this->texture_coords[0];
+    image_data.texture_coords[1] = this->texture_coords[1];
+}
 
-}
-}
 }
