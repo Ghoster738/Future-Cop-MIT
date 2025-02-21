6b05623bb5aa85220ddeaffb25a0821c01b18600
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-02-21 11:22:11 -0800

    Draw2D has OpenGL shaders now

--- src/Graphics/SDL2/GLES2/Internal/Draw2D.cpp
+++ src/Graphics/SDL2/GLES2/Internal/Draw2D.cpp
@@ -1,8 +1,111 @@
 #include "Draw2D.h"
 
+#include <iostream>
+
 namespace Graphics::SDL2::GLES2::Internal {
 
-Draw2D::Draw2D() {}
+const GLchar* Draw2D::default_vertex_shader =
+    // Vertex shader uniforms
+    "uniform mat4  Transform;\n" // projection * view * model.
+    "void main()\n"
+    "{\n"
+    "   texture_coord = TEXCOORD_0;\n"
+    "   vertex_color = COLOR_0;\n"
+    "   gl_Position = Transform * vec4(POSITION.xy, 0.0, 1.0);\n"
+    "}\n";
+
+const GLchar* Draw2D::default_fragment_shader =
+    "uniform sampler2D Texture;\n"
+    "void main()\n"
+    "{\n"
+    "    float visable = texture2D(Texture, texture_coord).r;\n"
+    "    if( visable < 0.03125 )\n"
+    "        discard;\n"
+    "    gl_FragColor = vertex_color;\n"
+    "}\n";
+
+const GLchar* Draw2D::getDefaultVertexShader() {
+    return default_vertex_shader;
+}
+
+int Draw2D::loadVertexShader( const char *const file_path ) {
+    return vertex_shader.loadShader( Shader::TYPE::VERTEX, file_path );
+}
+
+const GLchar* Draw2D::getDefaultFragmentShader() {
+    return default_fragment_shader;
+}
+
+int Draw2D::loadFragmentShader( const char *const file_path ) {
+    return fragment_shader.loadShader( Shader::TYPE::FRAGMENT, file_path );
+}
+
+int Draw2D::compileProgram() {
+    bool link_success     = true;
+    bool uniform_failed   = false;
+    bool attribute_failed = false;
+
+    // The two shaders should be allocated first.
+    if( vertex_shader.getType() == Shader::TYPE::VERTEX && fragment_shader.getType() == Shader::TYPE::FRAGMENT ) {
+
+        // Allocate the opengl program for the map.
+        program.allocate();
+
+        // Give the program these two shaders.
+        program.setVertexShader( &vertex_shader );
+        program.setFragmentShader( &fragment_shader );
+
+        // Link the shader
+        if( !program.link() )
+            link_success = false;
+        else
+        {
+            // Setup the uniforms for the map.
+            texture_uniform_id = program.getUniform( "Texture",   &std::cout, &uniform_failed );
+            matrix_uniform_id  = program.getUniform( "Transform", &std::cout, &uniform_failed );
+
+            attribute_failed |= !program.isAttribute(   "POSITION", &std::cout );
+            attribute_failed |= !program.isAttribute( "TEXCOORD_0", &std::cout );
+            attribute_failed |= !program.isAttribute(    "COLOR_0", &std::cout );
+        }
+
+        vertex_array.addAttribute( "POSITION",   2, GL_FLOAT,         false, sizeof( Vertex ), 0 );
+        vertex_array.addAttribute( "TEXCOORD_0", 2, GL_FLOAT,         false, sizeof( Vertex ), reinterpret_cast<void*>(2 * alignof( Vertex )) );
+        vertex_array.addAttribute( "COLOR_0",    4, GL_UNSIGNED_BYTE, true,  sizeof( Vertex ), reinterpret_cast<void*>(4 * alignof( Vertex )) );
+
+        vertex_array.allocate( program );
+        vertex_array.cullUnfound();
+
+        if( !link_success || uniform_failed || attribute_failed )
+        {
+            std::cout << "Font System Error\n"
+                << program.getInfoLog()
+                << "\nVertex shader log\n"
+                << vertex_shader.getInfoLog()
+                << "\nFragment shader log\n"
+                << fragment_shader.getInfoLog() << std::endl;
+
+            return 1;
+        }
+        else
+            return -1;
+
+        return 1;
+    }
+    else
+    {
+        return 0; // Not every shader was loaded.
+    }
+}
+
+Draw2D::Draw2D() {
+    attributes.push_back( Shader::Attribute( Shader::Type::HIGH,   "vec4 POSITION" ) );
+    attributes.push_back( Shader::Attribute( Shader::Type::MEDIUM, "vec2 TEXCOORD_0" ) );
+    attributes.push_back( Shader::Attribute( Shader::Type::LOW,    "vec4 COLOR_0" ) );
+
+    varyings.push_back( Shader::Varying( Shader::Type::LOW, "vec4 vertex_color" ) );
+    varyings.push_back( Shader::Varying( Shader::Type::MEDIUM, "vec2 texture_coord" ) );
+}
 
 Draw2D::~Draw2D() {}
 
@@ -46,5 +149,6 @@ void Draw2D::removeExternalImageData(const ExternalImage *const external_image_r
     if(search != this->external_images.end())
         this->external_images.erase(search);
 }
+
 
 }
--- src/Graphics/SDL2/GLES2/Internal/Draw2D.h
+++ src/Graphics/SDL2/GLES2/Internal/Draw2D.h
@@ -17,6 +17,37 @@ namespace Internal {
 
 class Draw2D {
 public:
+    struct Vertex {
+        float x, y;
+        float u, v;
+        uint32_t color_rgba;
+        Vertex( float set_x, float set_y, float set_u, float set_v,
+            uint32_t set_color_rgba ) :
+            x( set_x ), y( set_y ), u( set_u ), v( set_v ),
+            color_rgba( set_color_rgba )
+        {
+        }
+        void set( float set_x, float set_y, float set_u, float set_v,
+            uint32_t set_color_rgba ) {
+            x = set_x, y = set_y, u = set_u, v = set_v;
+            color_rgba = set_color_rgba;
+        }
+    };
+
+    const uint8_t DEFAULT_COLOR[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
+
+    struct Color {
+        uint32_t color_rgba;
+        uint8_t* toPointer() { return reinterpret_cast<uint8_t*>(&color_rgba); }
+        void set( uint8_t r, uint8_t b, uint8_t g, uint8_t a ) {
+            uint8_t *color = toPointer();
+            color[0] = r;
+            color[1] = b;
+            color[2] = g;
+            color[3] = a;
+        }
+    };
+
     struct ImageBaseData {
         glm::vec2 positions[2];
         glm::vec4 color;
@@ -32,13 +63,71 @@ public:
         Texture2D *texture_2d;
     };
 
-private:
+    static const GLchar* default_vertex_shader;
+    static const GLchar* default_fragment_shader;
+
+protected:
+    Program program;
+    std::vector<Shader::Attribute> attributes;
+    std::vector<Shader::Varying>   varyings;
+    Shader  vertex_shader;
+    Shader  fragment_shader;
+    VertexAttributeArray vertex_array;
+    GLuint texture_uniform_id;
+    GLuint matrix_uniform_id;
+
     std::map<const Texture2D *const, std::map<const Image *const, ImageData>> images;
     std::map<const ExternalImage *const, ExternalImageData> external_images;
 
 public:
     Draw2D();
     virtual ~Draw2D();
+
+    /**
+     * This method gets the default vertex shader depending on the GL version.
+     * @warning make sure the correct context is binded, or else you would get an improper shader.
+     * @return a pointer to a vertex shader.
+     */
+    static const GLchar* getDefaultVertexShader();
+
+    /**
+     * This method gets the default fragment shader depending on the GL version.
+     * @warning make sure the correct context is binded, or else you would get an improper shader.
+     * @return a pointer to a fragment shader.
+     */
+    static const GLchar* getDefaultFragmentShader();
+
+    /**
+     * This sets up and compiles this shader from memory.
+     * This is to be used for internal shaders.
+     * @param shader_source The memory pointer to the source code of the shader.
+     */
+    void setVertexShader( const GLchar *const shader_source = getDefaultVertexShader() );
+
+    /**
+     * This loads a shader from a text file, and compiles it.
+     * @param shader_source The memory pointer to the source code of the shader.
+     */
+    int loadVertexShader( const char *const file_path );
+
+    /**
+     * This sets up and compiles this shader from memory.
+     * This is to be used for internal shaders.
+     * @param shader_source The memory pointer to the source code of the shader.
+     */
+    void setFragmentShader( const GLchar *const shader_source = getDefaultFragmentShader() );
+
+    /**
+     * This loads a shader from a text file, and compiles it.
+     * @param shader_source The memory pointer to the source code of the shader.
+     */
+    int loadFragmentShader( const char *const file_path );
+
+    /**
+     * Link every shader to the program.
+     * @return false if one of the shaders are not loaded.
+     */
+    int compileProgram();
 
     /**
      * Draw all the images to be render
@@ -53,6 +142,8 @@ public:
     void updateExternalImageData(const ExternalImage *const external_image_r, const ExternalImageData& external_image_data);
 
     void removeExternalImageData(const ExternalImage *const external_image_r);
+
+    void clear();
 };
 
 }
