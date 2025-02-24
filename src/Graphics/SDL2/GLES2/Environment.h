83a0c89168c9516899ee1ec70bab4130c5075d8a
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-02-24 13:12:29 -0800

    ANMFrame is now testable

--- src/Graphics/SDL2/GLES2/Environment.h
+++ src/Graphics/SDL2/GLES2/Environment.h
@@ -19,7 +19,7 @@ namespace Graphics::SDL2::GLES2 {
 
 class Environment : public Graphics::Environment {
 public:
-    std::map<uint32_t, Data::Mission::ANMResource*> anm_resources;
+    std::map<uint32_t, const Data::Mission::ANMResource*> anm_resources;
     std::map<uint32_t, Internal::Texture2D*> textures;
     Internal::Texture2D          *shiney_texture_p; // This holds the environment map.
     Internal::World              *world_p; // This handles drawing the whole world.
