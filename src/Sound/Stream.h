9add2f35618dcb36fa1b7d0865d5114bea35766e
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-02-28 23:36:39 -0800

    Added documentation to Sound::Stream

--- src/Sound/Stream.h
+++ src/Sound/Stream.h
@@ -15,6 +15,13 @@ protected:
 public:
     virtual ~Stream() = 0;
 
+    /**
+     * Function to append the audio data to stream.
+     * @param interleaved_samples_r The pointer to the audio samples.
+     * @param num_of_channels The number of channels that interleaved_samples_r has.
+     * @param audio_samples_per_channel The samples per channel to the
+     * @return If the parameters match what was declared then return true. If this fails then return false.
+     */
     virtual bool appendSamples(float *interleaved_samples_r, unsigned num_of_channels, unsigned audio_samples_per_channel) = 0;
 
     /**
@@ -29,10 +36,19 @@ public:
      */
     virtual PlayerState getSpeakerState() const = 0;
 
+    /**
+     * @return The number of audio channels that the Video is using.
+     */
     virtual unsigned getNumOfChannels() const = 0;
 
+    /**
+     * @return The audio samples per channel of an **audio buffer** given to the stream by appendSamples method.
+     */
     virtual unsigned getSamplesPerChannel() const = 0;
 
+    /**
+     * @return The stream's frequency.
+     */
     virtual size_t getFrequency() const = 0;
 };
 
