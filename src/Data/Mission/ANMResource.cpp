711e3e4a1345f05ab703ce6e2eb3ce3582f5f17f
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-03-12 01:49:01 -0700

    ANM Resource also uses new pixel formats

--- src/Data/Mission/ANMResource.cpp
+++ src/Data/Mission/ANMResource.cpp
@@ -104,7 +104,7 @@ const std::filesystem::path Data::Mission::ANMResource::FILE_EXTENSION = "anm";
 const uint32_t              Data::Mission::ANMResource::IDENTIFIER_TAG = 0x63616E6D; // which is { 0x63, 0x61, 0x6E, 0x6D } or { 'c', 'a', 'n', 'm' } or "canm"
 
 Data::Mission::ANMResource::ANMResource() :
-    palette( Utilities::PixelFormatColor_R5G5B5A1() ), total_scanlines( 0 ), scanline_raw_bytes_p( nullptr )
+    palette( Utilities::PixelFormatColor_R5G5B5A1::linear ), total_scanlines( 0 ), scanline_raw_bytes_p( nullptr )
 {}
 
 Data::Mission::ANMResource::ANMResource( const ANMResource &obj ) : Resource( obj ), palette( obj.palette ), total_scanlines( obj.total_scanlines ), scanline_raw_bytes_p( nullptr )
@@ -239,8 +239,7 @@ Utilities::ImagePalette2D* Data::Mission::ANMResource::generateAnimationSheet( u
     Utilities::ImagePalette2D *animation_sheet_p = nullptr;
     
     if( rgba_palette ) {
-        auto rgba_color = Utilities::PixelFormatColor_R8G8B8A8();
-        Utilities::ColorPalette rgba_palette( rgba_color );
+        Utilities::ColorPalette rgba_palette( Utilities::PixelFormatColor_R8G8B8A8::linear );
         
         setColorPalette( rgba_palette );
 
@@ -297,15 +296,14 @@ int Data::Mission::ANMResource::write( const std::filesystem::path& file_path, c
         unsigned video_frames = getTotalScanlines() / Video::SCAN_LINE_POSITIONS;
         
         // Make a color palette that holds RGBA values
-        auto rgba_color = Utilities::PixelFormatColor_R8G8B8A8();
-        Utilities::ColorPalette rgba_palette( rgba_color );
+        Utilities::ColorPalette rgba_palette( Utilities::PixelFormatColor_R8G8B8A8::linear );
         
         setColorPalette( rgba_palette );
         
         // This contains a list of frames of this file format.
         Utilities::ImagePalette2D image_sheet( Video::WIDTH, Video::HEIGHT * video_frames, rgba_palette );
 
-        Utilities::ImageFormat::ImageFormat* the_choosen_r = chooser.getWriterReference( Utilities::PixelFormatColor_R8G8B8A8() );
+        Utilities::ImageFormat::ImageFormat* the_choosen_r = chooser.getWriterReference( Utilities::PixelFormatColor_R8G8B8A8::linear );
 
         if( iff_options.anm.shouldWrite( iff_options.enable_global_dry_default ) && the_choosen_r != nullptr ) {
             Utilities::Buffer buffer;
