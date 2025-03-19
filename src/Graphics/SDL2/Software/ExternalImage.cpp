f5a172c9c9e9b2895b73bc03d72d1d4b349cb197
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-03-19 11:19:24 -0700

    ExternalImage now gives data.

--- src/Graphics/SDL2/Software/ExternalImage.cpp
+++ src/Graphics/SDL2/Software/ExternalImage.cpp
@@ -4,13 +4,62 @@
 
 namespace Graphics::SDL2::Software {
 
-Graphics::ExternalImage* Environment::allocateExternalImage(bool has_alpha) { return nullptr; }
+Graphics::ExternalImage* Environment::allocateExternalImage(bool has_alpha) {
+    Software::ExternalImage *image_p;
+
+    if(has_alpha) {
+        image_p = new Graphics::SDL2::Software::ExternalImage(Utilities::PixelFormatColor_R8G8B8A8::linear);
+        image_p->has_alpha_channel = true;
+        this->external_image_draw_2d.images.insert( image_p );
+    }
+    else {
+        image_p = new Graphics::SDL2::Software::ExternalImage(Utilities::PixelFormatColor_R8G8B8::linear);
+        image_p->has_alpha_channel = false;
+        this->external_image_draw_2d.opaque_images.insert( image_p );
+    }
+
+    // ImageBase
+    image_p->color        = glm::vec4( 1.0,  1.0,  1.0,  1.0);
+    image_p->positions[0] = glm::vec2(  0.0,   0.0);
+    image_p->positions[1] = glm::vec2( 16.0,  16.0);
+    image_p->is_visable   = false;
+
+    image_p->internal.color        = image_p->color;
+    image_p->internal.positions[0] = image_p->positions[0];
+    image_p->internal.positions[1] = image_p->positions[1];
+    image_p->internal.is_visable   = image_p->is_visable;
+
+    // OpenGL Info
+    image_p->environment_r = this;
+
+    return image_p;
+}
 
 ExternalImage::ExternalImage(const Utilities::PixelFormatColor &color) : Graphics::ExternalImage(color) {}
 
 ExternalImage::~ExternalImage() {}
 
-void ExternalImage::update() {}
+void ExternalImage::update() {
+    this->internal.color        = this->color;
+    this->internal.positions[0] = this->positions[0];
+    this->internal.positions[1] = this->positions[1];
+    this->internal.is_visable   = this->is_visable;
+
+    if(!this->has_alpha_channel) {
+        if(this->color.a != this->internal.color.a) {
+            if(this->color.a < 1.0f) {
+                this->environment_r->external_image_draw_2d.opaque_images.erase( this );
+                this->environment_r->external_image_draw_2d.images.insert( this );
+            }
+            else {
+                this->environment_r->external_image_draw_2d.images.erase( this );
+                this->environment_r->external_image_draw_2d.opaque_images.insert( this );
+            }
+        }
+    }
+    else
+        this->environment_r->external_image_draw_2d.images.insert( this );
+}
 
 void ExternalImage::upload() {}
 
--- src/Graphics/SDL2/Software/ExternalImage.h
+++ src/Graphics/SDL2/Software/ExternalImage.h
@@ -11,6 +11,14 @@ class ExternalImage : public Graphics::ExternalImage {
 public:
     Environment *environment_r;
 
+    struct {
+        glm::vec4 color; // The color of the image.
+        glm::vec2 positions[2]; // Screen coordinates
+        bool      is_visable; // Determines whether the image is visable.
+    } internal;
+
+    bool has_alpha_channel;
+
     ExternalImage(const Utilities::PixelFormatColor &color);
 
     virtual ~ExternalImage();
