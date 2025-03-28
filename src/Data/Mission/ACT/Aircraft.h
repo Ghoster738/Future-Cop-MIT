f99178ea70f4d94ad90f557c1c2ff69d8919a78b
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-03-28 16:12:16 -0700

    Renamed variables

--- src/Data/Mission/ACT/Aircraft.h
+++ src/Data/Mission/ACT/Aircraft.h
@@ -20,24 +20,24 @@ public:
     static uint_fast8_t TYPE_ID;
 
     struct Internal {
-        uint8_t uint8_10;
-        uint8_t uint8_11;
+        uint8_t uint8_0;
+        uint8_t uint8_1;
+        uint16_t uint16_0;
+        uint8_t uint8_2;
+        uint8_t uint8_3;
+        uint16_t uint16_1;
+        uint16_t uint16_2;
+        uint16_t uint16_3;
+        uint16_t uint16_4;
+        uint16_t uint16_5;
+        uint16_t uint16_6;
         uint16_t uint16_7;
-        uint8_t uint8_12;
-        uint8_t uint8_13;
         uint16_t uint16_8;
         uint16_t uint16_9;
         uint16_t uint16_10;
         uint16_t uint16_11;
         uint16_t uint16_12;
         uint16_t uint16_13;
-        uint16_t uint16_14;
-        uint16_t uint16_15;
-        uint16_t uint16_16;
-        uint16_t uint16_17;
-        uint16_t uint16_18;
-        uint16_t uint16_19;
-        uint16_t uint16_20;
     } internal;
 
 protected:
