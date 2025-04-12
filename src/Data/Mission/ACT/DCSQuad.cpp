3065811421a36b378f997121e172eee9da65cdbc
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-04-12 01:36:17 -0700

    DCSQuad is my given name from them.
    I just feel like they are more descriptive tahn "Fog".

---
+++ src/Data/Mission/ACT/DCSQuad.cpp
@@ -0,0 +1,86 @@
+#include "DCSQuad.h"
+
+#include <cassert>
+
+uint_fast8_t Data::Mission::ACT::DCSQuad::TYPE_ID = 97;
+
+Json::Value Data::Mission::ACT::DCSQuad::makeJson() const {
+    Json::Value root = Data::Mission::ACTResource::makeJson();
+    const std::string NAME = getTypeIDName();
+
+    root["ACT"][NAME]["tags"]          = internal.tags;
+    root["ACT"][NAME]["dcs_id"]        = internal.dcs_id;
+    root["ACT"][NAME]["height_offset"] = internal.height_offset;
+    root["ACT"][NAME]["width"]         = internal.width;
+    root["ACT"][NAME]["height"]        = internal.height;
+    root["ACT"][NAME]["y_rotation"]    = internal.y_rotation;
+    root["ACT"][NAME]["z_rotation"]    = internal.z_rotation;
+    root["ACT"][NAME]["x_rotation"]    = internal.x_rotation;
+    root["ACT"][NAME]["uint8_2"]       = internal.uint8_2;
+    root["ACT"][NAME]["red"]           = internal.red;
+    root["ACT"][NAME]["green"]         = internal.green;
+    root["ACT"][NAME]["blue"]          = internal.blue;
+    root["ACT"][NAME]["zero"]          = internal.zero;
+
+    return root;
+}
+
+bool Data::Mission::ACT::DCSQuad::readACTType( uint_fast8_t act_type, Utilities::Buffer::Reader &data_reader, Utilities::Buffer::Endian endian ) {
+    assert(act_type == this->getTypeID());
+
+    if( data_reader.totalSize() != this->getSize() )
+        return false;
+
+    internal.tags = data_reader.readU8(); // Values: 0, 2, 16, 18,
+    internal.dcs_id = data_reader.readU8(); // Values: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 27, 28, 29, 31, 32, 35, 36,
+    internal.height_offset = data_reader.readU16( endian ); // Values: 0, 4, 40, 61, 122, 163, 204, 245, 266, 286, 307, 327, 348, 368, 389, 409, 430, 450, 491, 512, 528, 532, 573, 593, 614, 634, 655, 675, 696, 716, 737, 757, 819, 860, 901, 942, 1024, 1064, 1105, 1126, 1167, 1208, 1228, 1269, 1310, 1392, 1433, 1536, 1638, 1720, 64512,
+    internal.width = data_reader.readU16( endian ); // Values: 102, 163, 204, 245, 286, 307, 327, 368, 409, 491, 512, 532, 696, 819, 1024, 1228, 1351, 1433, 1638, 1761, 1802, 1843, 1925, 1966, 2007, 2048, 2375, 2457, 2662, 2744, 2867, 2990, 3072, 3276,
+    internal.height = data_reader.readU16( endian ); // Values: 40, 204, 286, 307, 327, 348, 380, 409, 491, 512, 532, 614, 819, 983, 1024, 1228, 1351, 1433, 1638, 1761, 1802, 1843, 1925, 1966, 2007, 2048, 2375, 2457, 2662, 2744, 2867, 2990, 3072, 3276,
+    internal.y_rotation = data_reader.readU16( endian ); // Values: 0, 30, 110, 512, 545, 800, 1024, 1200, 1235, 1280, 1333, 1536, 1793, 2000, 2048, 2200, 2222, 2560, 2750, 2900, 3072, 3333,
+    internal.z_rotation = data_reader.readU16( endian ); // Values: 0, 200, 512, 1024, 2900, 3072, 3100, 3400, 3500, 3600, 3900,
+    internal.x_rotation = data_reader.readU16( endian ); // Values: 0, 1024, 3072,
+    internal.uint8_2 = data_reader.readU8(); // Values: 0, 1, 3, 
+    internal.red = data_reader.readU8(); // Values: 0, 16, 20, 30, 32, 33, 34, 35, 40, 44, 50, 54, 64, 80, 84, 100, 128,
+    internal.green = data_reader.readU8(); // Values: 16, 20, 28, 30, 32, 34, 40, 50, 62, 64, 75, 80, 82, 92, 96, 100, 120, 128,
+    internal.blue = data_reader.readU8(); // Values: 0, 8, 16, 20, 21, 22, 24, 25, 30, 32, 34, 40, 50, 64, 80, 100, 128,
+    internal.zero = data_reader.readU16( endian ); // Always 0
+
+    return true;
+}
+
+Data::Mission::ACT::DCSQuad::DCSQuad() {}
+
+Data::Mission::ACT::DCSQuad::DCSQuad( const ACTResource& obj ) : ACTResource( obj ) {}
+
+Data::Mission::ACT::DCSQuad::DCSQuad( const DCSQuad& obj ) : ACTResource( obj ), internal( obj.internal ) {}
+
+uint_fast8_t Data::Mission::ACT::DCSQuad::getTypeID() const {
+    return TYPE_ID;
+}
+
+std::string Data::Mission::ACT::DCSQuad::getTypeIDName() const {
+    return "DCSQuad";
+}
+
+size_t Data::Mission::ACT::DCSQuad::getSize() const {
+    return 20; // bytes
+}
+
+bool Data::Mission::ACT::DCSQuad::checkRSL() const { return false; }
+
+Data::Mission::Resource* Data::Mission::ACT::DCSQuad::duplicate() const {
+    return new Data::Mission::ACT::DCSQuad( *this );
+}
+
+Data::Mission::ACTResource* Data::Mission::ACT::DCSQuad::duplicate( const ACTResource &original ) const {
+    auto copy_r = dynamic_cast<const DCSQuad*>( &original );
+
+    if( copy_r != nullptr )
+        return new Data::Mission::ACT::DCSQuad( *copy_r );
+    else
+        return new Data::Mission::ACT::DCSQuad( original );
+}
+
+Data::Mission::ACT::DCSQuad::Internal Data::Mission::ACT::DCSQuad::getInternal() const {
+    return internal;
+}
---
+++ src/Data/Mission/ACT/DCSQuad.h
@@ -0,0 +1,62 @@
+#ifndef DATA_MISSION_ACTOR_ID_97_HEADER
+#define DATA_MISSION_ACTOR_ID_97_HEADER
+
+#include "../ACTResource.h"
+#include <json/json.h>
+
+namespace Data {
+
+namespace Mission {
+
+namespace ACT {
+
+class DCSQuad : public ACTResource {
+public:
+    static uint_fast8_t TYPE_ID;
+
+    struct Internal {
+        uint8_t tags;
+        uint8_t dcs_id;
+        uint16_t height_offset;
+        uint16_t width;
+        uint16_t height;
+        uint16_t y_rotation;
+        uint16_t z_rotation;
+        uint16_t x_rotation;
+        uint8_t uint8_2; // Ground Cast?
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+        uint16_t zero;
+    } internal;
+
+protected:
+    virtual Json::Value makeJson() const;
+    virtual bool readACTType( uint_fast8_t act_type, Utilities::Buffer::Reader &data_reader, Utilities::Buffer::Endian endian );
+
+public:
+    DCSQuad();
+    DCSQuad( const ACTResource& obj );
+    DCSQuad( const DCSQuad& obj );
+
+    virtual uint_fast8_t getTypeID() const;
+    virtual std::string getTypeIDName() const;
+
+    virtual size_t getSize() const;
+
+    virtual bool checkRSL() const;
+
+    virtual Resource* duplicate() const;
+
+    virtual ACTResource* duplicate( const ACTResource &original ) const;
+
+    Internal getInternal() const;
+
+};
+}
+
+}
+
+}
+
+#endif // DATA_MISSION_ACTOR_ID_97_HEADER
