d44ea70baec5c62eb919c77868e50402c0302a1e
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-04-13 13:32:53 -0700

    Render Modes for DCSQuads have been implemented

--- src/Graphics/SDL2/GLES2/Internal/ParticleDraw.cpp
+++ src/Graphics/SDL2/GLES2/Internal/ParticleDraw.cpp
@@ -177,7 +177,20 @@ void ParticleDraw::draw(Graphics::SDL2::GLES2::Camera& camera) {
             draw_triangles_r[ index ].vertices[x].vertex_metadata = glm::i16vec2(0, 0);
         }
 
-        draw_triangles_r[ index ].setup( cbmp_id, camera_position, DynamicTriangleDraw::PolygonType::ADDITION );
+        DynamicTriangleDraw::PolygonType polygon_type;
+
+        if(instance_data.is_opaque) {
+            polygon_type = DynamicTriangleDraw::PolygonType::MIX;
+        }
+        else {
+            if(instance_data.is_addition) {
+                polygon_type = DynamicTriangleDraw::PolygonType::ADDITION;
+            }
+            else
+                polygon_type = DynamicTriangleDraw::PolygonType::MIX;
+        }
+
+        draw_triangles_r[ index ].setup( cbmp_id, camera_position, polygon_type );
         draw_triangles_r[ index ] = draw_triangles_r[ index ].addTriangle( camera_position, camera_3D_model_transform );
 
         draw_triangles_r[ index + 1 ] = draw_triangles_r[ index ];
